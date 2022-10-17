1caba4117255994ce7d170c5b0cdd1da2f2315e2
Edward Han <edwardhuahan@gmail.com>
2022-10-17 19:03:06 -0400

    Initialized projects with frontend and backend

---
+++ client/src/App.tsx
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>{!data ? "Loading..." : data}</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
