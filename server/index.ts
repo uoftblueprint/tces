cf27f0f7a5d50b9c6056f996f94b61ea1170bec3
Jordan Janakievski <jordanjanakievski@Jordans-MacBook-Pro.local>
2022-11-08 15:11:56 -0500

    Added POST req test for server; Attempt at handling CORS

--- server/index.ts
+++ server/index.ts
@@ -1,13 +1,40 @@
 // server/index.js
 
 const express = require("express");
+const bodyParser = require("body-parser")
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+// Trying to handle CORS
+app.use((req, res, next) => {
+  const allowedOrigins = ['http://localhost:3006'];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+       res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', true);
+  return next();
+});
+
+// Allow parsing of JSON data
+app.use(bodyParser.json());
+
 app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
+});
+
+// Test POST request
+app.post("/api", (req, res) => {
+    const request = req.body;
+    console.log(request);
+    if (!request) {
+        return res.status(400).json({ message: "No request body" });;
+    }
+    res.json({ message: "Data received!"});
 });
 
 app.listen(PORT, () => {
