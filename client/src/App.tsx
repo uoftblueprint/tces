9965bd5d0269f1fc8b5cefccac8f3a5cbfd06486
manaljav <manaljav0425@gmail.com>
2022-11-13 14:47:21 -0500

    test navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import { Button } from "@mui/material";
+import  SettingsIcon  from "@mui/icons-material/Settings";
+import { NavBar } from './NavBar/NavBar';
 function App() {
   const [data, setData] = useState(null);
 
@@ -13,9 +15,9 @@ function App() {
 
   return (
     <div className="App">
+      <NavBar/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
       </header>
     </div>
   );
