14dad1cfd46b8e36b7d6fd07971559a945a8e639
Jordan Janakievski <jordanjanakievski@Jordans-MacBook-Pro.local>
2022-11-15 21:27:47 -0500

    Set up winston logger package

---
+++ server/logger/index.ts
@@ -0,0 +1,8 @@
+const winston = require("winston");
+
+const logger = winston.createLogger({
+	transports: [
+		new winston.transports.Console(),
+		new winston.transports.File({ filename: "combined.log" }),
+	],
+});
