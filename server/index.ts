a996bf3fc5650d4a1c6be57bbd8e4f78ac4b381c
thardchi <thardchi.tutor@gmail.com>
2022-11-20 09:52:45 -0500

    installed jest libray and wrote test to verify that response from app.get endpoint is Hello from server

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,5 @@
 // server/index.js
+import { expect, test } from '@jest/globals';
 
 const express = require("express");
 
@@ -13,3 +14,15 @@ app.get("/api", (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
 });
+
+
+const request = require("jest");
+const endpoint = require("../app.get");
+
+describe("GET / ", () => {
+    test("should respond with Hello from server!", async () => {
+        const response = await request(endpoint).get("/");
+        expect(response.body).toEqual("Hello from server!");
+        expect(response.statusCode).toBe(200);
+    })
+})
