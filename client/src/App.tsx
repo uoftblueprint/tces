67aa30f16dce56c239cc51045c747b81ae9caf9a
thardchi <thardchi.tutor@gmail.com>
2022-11-19 22:08:46 -0500

    implemented activityTimeline function from MUI website. Also installed Material UI and MUI Lab

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,42 @@ import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+//importing files needed to implement activityTimeline function
+import Timeline from '@mui/lab/Timeline';
+import TimelineItem from '@mui/lab/TimelineItem';
+import TimelineSeparator from '@mui/lab/TimelineSeparator';
+import TimelineConnector from '@mui/lab/TimelineConnector';
+import TimelineContent from '@mui/lab/TimelineContent';
+import TimelineDot from '@mui/lab/TimelineDot';
+
+//implementing activityTimeline function
+export default function BasicTimeline() {
+  return (
+    <Timeline>
+      <TimelineItem>
+        <TimelineSeparator>
+          <TimelineDot />
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>Eat</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineSeparator>
+          <TimelineDot />
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>Code</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineSeparator>
+          <TimelineDot />
+        </TimelineSeparator>
+        <TimelineContent>Sleep</TimelineContent>
+      </TimelineItem>
+    </Timeline>
+  );
+}
+
 function App() {
   const [data, setData] = useState(null);
 
@@ -21,4 +57,4 @@ function App() {
   );
 }
 
-export default App;
+//export default App;
