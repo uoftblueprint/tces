6a43e67a92336eff41d9b87ef35bd44a479fa9f7
Jordan Janakievski <jajanak10@gmail.com>
2022-11-22 21:35:32 -0500

    Created logger format and export; Created README for the logger

--- server/logger/index.ts
+++ server/logger/index.ts
@@ -1,8 +1,31 @@
-const winston = require("winston");
+const { createLogger, format, transports } = require("winston");
+const { combine, timestamp, printf } = format;
 
-const logger = winston.createLogger({
-	transports: [
-		new winston.transports.Console(),
-		new winston.transports.File({ filename: "combined.log" }),
-	],
+const formatSetup = printf(({ level, message, timestamp }) => {
+	return `${timestamp} ${level}: ${message}`;
 });
+
+module.exports = (dirname) => {
+	const levels = {
+		error: 0,
+		warn: 1,
+		info: 2,
+		http: 3,
+		verbose: 4,
+		debug: 5,
+	};
+
+	for (let level of Object.keys(levels)) {
+		levels[level] = createLogger({
+			levels: { [level]: 0 },
+			format: combine(timestamp(), formatSetup),
+			transports: [
+				new transports.Console({
+					level,
+				}),
+			],
+		})[level];
+	}
+
+	return levels;
+};
