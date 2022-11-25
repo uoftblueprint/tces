67e846667ffcd0e9f8f57fde647e6c562db0d35b
Kenneth Miura <kenneth.t.miura@gmail.com>
2022-11-24 19:34:41 -0500

    refactor DB to be separate from index

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response } from 'express';
+import { connectToDatabase } from './database/conn';
 
 const PORT = process.env.PORT || 3001;
 
@@ -7,37 +8,30 @@ const app = express();
 // Allow parsing of JSON data
 app.use(express.json());
 
-app.get("/api", (req: Request, res: Response) => {
-	res.json({ message: "Hello from server! NEW HOT " });
-});
 
-// Test POST request
-app.post("/api", (req:Request, res: Response) => {
-	const request = req.body;
-	if (!request) {
-		return res.status(400).json({ message: "No request body" });
-	}
-	res.json({ message: "Data received! " + request.message });
-});
+connectToDatabase()
+  .then(() => {
+    app.get("/api", (req: Request, res: Response) => {
+      res.json({ message: "Hello from server!" });
+    });
 
-app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`);
-});
+    // Test POST request
+    app.post("/api", (req:Request, res: Response) => {
+      const request = req.body;
+      if (!request) {
+        return res.status(400).json({ message: "No request body" });
+      }
+      res.json({ message: "Data received! " + request.message });
+    });
 
-const { MongoClient } = require("mongodb");
-const uri = "mongodb://mongo:27017";  // Connection URI
-const client = new MongoClient(uri);  // Create a new MongoClient
-
-async function run() {
-  try {
-    await client.connect();  // Connect the client to the server
-    await client.db("admin").command({ ping: 1 });  // Establish and verify connection
-    console.log("Connected successfully to database server");
-  } catch(error) {
-    console.error("Database connection failed", error);
-  } finally {
-    await client.close();  // Ensures that the client will close when you finish/error
+    app.listen(PORT, () => {
+        console.log(`Server listening on ${PORT}`);
+    });
+  })
+  .catch((error: Error) => {
+    console.error("Database Connection Failed", error)
+    process.exit()
   }
-}
+  )
 
-//run().catch(console.dir);
+
