ce398f2125473cd65288a556225fd18e5ebb2b74
Annie <liuzr.annie@gmail.com>
2022-11-27 17:06:28 -0500

    main struct

---
+++ client/src/JobCard/JobCard.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+
+import "./JobCardData.json"
+import "./JobCard.css"
+
+export const JobCard = (  ) => {
+
+    return (
+        <div className='cardBody'>
+
+            
+        </div>
+    )
+}
+
+export default JobCard
