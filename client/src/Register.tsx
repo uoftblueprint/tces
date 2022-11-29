7a08b1e2ea0d29d034cf9e913f624b16a0b43002
Ganesh Asapu <62232361+ganeshasapu@users.noreply.github.com>
2022-11-29 17:03:55 -0500

    removed unused imports

--- client/src/Register.tsx
+++ client/src/Register.tsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
-import TextField from "@mui/material/TextField";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Checkbox from "@mui/material/Checkbox";
 import { Link } from "react-router-dom";
