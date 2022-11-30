a4bfe15b97abd378c61ac2be56163bc9d00e7483
Helen Li <hallenljy@gmail.com>
2022-11-29 22:50:13 -0500

    revert accident merge with main

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 import Communication from "./components/Communication";
 
 function App() {
