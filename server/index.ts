2237848b26e99a136af764ac9071a4fa2a7e57df
Helen Li <hallenljy@gmail.com>
2022-12-04 17:24:58 -0500

    working hopefully

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,4 @@
 // server/index.js
-import { expect, test } from '@jest/globals';
-
 const express = require("express");
 
 const PORT = process.env.PORT || 3001;
@@ -14,15 +12,3 @@ app.get("/api", (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
 });
-
-
-const request = require("jest");
-const endpoint = require("../app.get");
-
-describe("GET / ", () => {
-    test("should respond with Hello from server!", async () => {
-        const response = await request(endpoint).get("/");
-        expect(response.body).toEqual("Hello from server!");
-        expect(response.statusCode).toBe(200);
-    })
-})
