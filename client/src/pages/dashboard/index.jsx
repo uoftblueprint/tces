c0a97075fb34cf1d761b42c435c49bb903f6ac11
Kevin Le <74465053+kevinle623@users.noreply.github.com>
2023-10-24 00:02:33 -0400

    some progress

---
+++ client/src/pages/dashboard/index.jsx
@@ -0,0 +1,36 @@
+import {
+  Button,
+  Typography,
+  Container,
+  Box,
+  Grid,
+} from '@mui/material';
+
+function Dashboard() {
+  const firstName = 'Kevin Le';
+  return (
+    <Container>
+      <Box my={4}>
+        <Grid container justifyContent="space-between" alignItems="center">
+          <Grid item>
+            <Typography variant="h4" gutterBottom>
+              {'Welcome Back, '}
+              {firstName}
+              !
+            </Typography>
+            <Typography variant="body1" gutterBottom>
+              Start a search, add a new entry using the button or browse through the timelines.
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Button variant="contained" color="primary">
+              Add New Entry
+            </Button>
+          </Grid>
+        </Grid>
+      </Box>
+    </Container>
+  );
+}
+
+export default Dashboard;
