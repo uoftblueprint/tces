71c474bca856a3fb934ebed5a8b32b9afce95e72
Kevin Le <74465053+kevinle623@users.noreply.github.com>
2023-10-24 00:37:40 -0400

    fixed prettier

--- client/src/pages/dashboard/index.jsx
+++ client/src/pages/dashboard/index.jsx
@@ -1,25 +1,18 @@
-import {
-  Button,
-  Typography,
-  Container,
-  Box,
-  Grid,
-} from '@mui/material';
+import { Button, Typography, Container, Box, Grid } from "@mui/material";
 
 function Dashboard() {
-  const firstName = 'Kevin Le';
+  const firstName = "Kevin Le";
   return (
     <Container>
       <Box my={4}>
         <Grid container justifyContent="space-between" alignItems="center">
           <Grid item>
             <Typography variant="h4" gutterBottom>
-              {'Welcome Back, '}
-              {firstName}
-              !
+              {"Welcome Back, "} {firstName}!
             </Typography>
             <Typography variant="body1" gutterBottom>
-              Start a search, add a new entry using the button or browse through the timelines.
+              Start a search, add a new entry using the button or browse through
+              the timelines.
             </Typography>
           </Grid>
           <Grid item>
