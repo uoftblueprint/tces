9ef0423980971070d569f5bd57f2876d17676a74
VangoCode <ronvarshavsky1@gmail.com>
2023-10-25 14:29:14 -0400

    TCES-12 feat(auth) add PassportJS packages

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+// PassportJS imports for auth
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+// Encryption library
+const crypto = require('crypto');
+
+
 // User logs in with password
 router.post('/login/password', (req, res) => {
     res.status(200).send("TODO: Implement login functionality");
