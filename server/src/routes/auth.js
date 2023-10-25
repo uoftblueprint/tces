1e80da7119f7fba0ed64ea015e56dccbc383c203
VangoCode <ronvarshavsky1@gmail.com>
2023-10-25 11:09:14 -0400

    TCES-12 fix(auth): send response on post request

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -2,18 +2,18 @@ const express = require('express');
 const router = express.Router();
 
 // User logs in with password
-router.post('/login/password', () => {
-    return "TODO: Implement login functionality";
+router.post('/login/password', (req, res) => {
+    res.status(200).send("TODO: Implement login functionality");
 });
 
 // User signs up
-router.post('/signup', () => {
-    return "TODO: Implement signup functionality";
+router.post('/signup', (req, res) => {
+    res.status(200).send("TODO: Implement signup functionality");
 });
 
 // User logs out
-router.post('/logout', () => {
-    return "TODO: Implement logout functionality";
+router.post('/logout', (req, res) => {
+    res.status(200).send("TODO: Implement logout functionality");
 });
 
 module.exports = router;
