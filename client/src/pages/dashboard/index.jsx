3a0e24e5ec6a4e132b115148e11350af0a03d98e
Kevin Le <74465053+kevinle623@users.noreply.github.com>
2023-10-26 23:59:12 -0400

    fixed top section

--- client/src/pages/dashboard/index.jsx
+++ client/src/pages/dashboard/index.jsx
@@ -1,4 +1,13 @@
-import { Button, Typography, Container, Box, Grid } from "@mui/material";
+import {
+  ButtonGroup,
+  Button,
+  Typography,
+  Container,
+  Box,
+  Grid,
+} from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 
 function Dashboard() {
   const firstName = "Kevin Le";
@@ -7,18 +16,28 @@ function Dashboard() {
       <Box my={4}>
         <Grid container justifyContent="space-between" alignItems="center">
           <Grid item>
-            <Typography variant="h4" gutterBottom>
+            <Typography variant="h4" gutterBottom align="left">
               {"Welcome Back, "} {firstName}!
             </Typography>
-            <Typography variant="body1" gutterBottom>
+
+            <Typography variant="body1" gutterBottom align="left">
               Start a search, add a new entry using the button or browse through
               the timelines.
             </Typography>
           </Grid>
           <Grid item>
-            <Button variant="contained" color="primary">
-              Add New Entry
-            </Button>
+            <Grid item>
+              <ButtonGroup
+                variant="contained"
+                color="primary"
+                aria-label="split button"
+              >
+                <Button startIcon={<AddIcon />}>Add New Entry</Button>
+                <Button>
+                  <ArrowDropDownIcon />
+                </Button>
+              </ButtonGroup>
+            </Grid>
           </Grid>
         </Grid>
       </Box>
