426d4bb9387958d09addc195c84a1efa7c0ec1a6
VangoCode <ronvarshavsky1@gmail.com>
2023-10-26 23:25:16 -0400

    TCES-12 refactor(create_user) fix indentation

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -16,18 +16,18 @@ const { frontendUrl } = require('../configs/frontend_config');
 
 // Local strategy configuration
 passport.use(new LocalStrategy(function verify(username, password, cb) {
-  connection.get('SELECT * FROM users WHERE email = ?', [ username ], function(err, row) {
-    if (err) { return cb(err); }
-    if (!row) { return cb(null, false, { message: 'Incorrect username or password.' }); }
+    connection.get('SELECT * FROM users WHERE email = ?', [ username ], function(err, row) {
+        if (err) { return cb(err); }
+        if (!row) { return cb(null, false, { message: 'Incorrect username or password.' }); }
 
-    crypto.pbkdf2(password, row.salt, 310000, 32, 'sha256', function(err, hashedPassword) {
-      if (err) { return cb(err); }
-      if (!crypto.timingSafeEqual(row.hashed_password, hashedPassword)) {
-        return cb(null, false, { message: 'Incorrect username or password.' });
-      }
-      
-      return cb(null, row);
-      });
+        crypto.pbkdf2(password, row.salt, 310000, 32, 'sha256', function(err, hashedPassword) {
+            if (err) { return cb(err); }
+            if (!crypto.timingSafeEqual(row.hashed_password, hashedPassword)) {
+                return cb(null, false, { message: 'Incorrect username or password.' });
+            }
+        
+            return cb(null, row);
+        });
     });
 }));
 
@@ -50,30 +50,35 @@ router.post('/login/password',
  * @type string {body.password} 
  */
 router.post('/create_user', (req, res, next) => {
-    // Generate salt value
-    const salt = crypto.randomBytes(16);
+    try {
+        // Generate salt value
+        const salt = crypto.randomBytes(16);
 
-    crypto.pbkdf2(req.body.password, salt, 310000, 32, 'sha256', (err, hashedPassword) => {
-        if (err) { return next(err); } 
+        crypto.pbkdf2(req.body.password, salt, 310000, 32, 'sha256', (err, hashedPassword) => {
+            if (err) { return next(err); } 
 
-        connection.query(`INSERT INTO ${userTable} (first_name, last_name, email, password, salt) VALUES (?, ?, ?, ?, ?)`, [
-            req.body.first_name,
-            req.body.last_name,
-            req.body.email,
-            hashedPassword,
-            salt
-        ], (err) => {
-            if (err) { return next(err); }
-            var user = {
-                id: this.lastID,
-                email: req.body.email
-            };
-            req.login(user, (err) => {
-            if (err) { return next(err); }
-                res.redirect('/');
+            connection.query(`INSERT INTO ${userTable} (first_name, last_name, email, password, salt) VALUES (?, ?, ?, ?, ?)`, [
+                req.body.first_name,
+                req.body.last_name,
+                req.body.email,
+                hashedPassword,
+                salt
+            ], (err) => {
+                if (err) { return next(err); }
+                var user = {
+                    id: this.lastID,
+                    email: req.body.email
+                };
+                req.login(user, (err) => {
+                if (err) { return next(err); }
+                    res.redirect('/');
+                });
             });
         });
-    });
+    } catch (err) {
+        console.log(err);
+        res.status(500).send("error");
+    }
 });
 
 // User logs out
