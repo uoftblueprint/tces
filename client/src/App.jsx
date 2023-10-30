c7a31381817efca7bb403bc45a65fdd0d851fecc
Taha <tsid2181@gmail.com>
2023-10-30 11:19:32 -0400

    Rendering Create and Edit User

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,15 @@
 import "./App.css";
 import DashboardPage from "./pages/dashboard";
 // import LoginPage from "./pages/login";
+import CreatePage from "./components/Create";
+import EditPage from "./components/Edit";
 
 function App() {
   return (
     <div className="App">
       <DashboardPage />
+      <CreatePage />
+      <EditPage />
     </div>
   );
 }
