4ee51ebfaa440d77ec4948d9af30ba27506b13d5
3m1ly03 <emilyfaithzhou@icloud.com>
2023-10-30 19:29:58 -0400

    TCES-10 Playing around with endpoint

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { useState } from 'react';
 
 const Container = styled.div`
     height: 100vh;
@@ -119,6 +120,41 @@ const Button = styled.button`
 `;
 
 function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = () => {
+        // Create a JSON object with the required keys and values
+        const loginData = {
+            username: email,
+            password,
+        };
+
+        // Convert the JSON object to a string
+        const loginDataJSON = JSON.stringify(loginData);
+
+        // Replace url with target route
+        fetch('http://localhost:8000/auth/login/password', {
+            method: 'POST', // Not sure which method
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: loginDataJSON,
+        });
+        // .then((response) => {
+        //     if (response.ok) {
+        //         // Handle success response (e.g., redirect or show a success message)
+        //         console.log('Login successful');
+        //     } else {
+        //         // Handle error response (e.g., show an error message)
+        //         console.error('Login failed');
+        //     }
+        // })
+        // .catch((error) => {
+        //     console.error('Error:', error);
+        // });
+    };
+
     return (
         <Container>
             <Logo src='./img/tcesLogo.svg' alt='TCES Logo' />
@@ -127,10 +163,21 @@ function Login() {
                 <P>Log in below with your details.</P>
             </MessageContainer>
             <InputContainer>
-                <Input placeholder='TCES Email' name='tces email' />
-                <Input placeholder='Password' name='password' />
+                <Input
+                    placeholder='TCES Email'
+                    name='tces email'
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <Input
+                    type='password'
+                    placeholder='Password'
+                    name='password'
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
             </InputContainer>
-            <Button>LOG IN</Button>
+            <Button onClick={handleLogin}>LOG IN</Button>
         </Container>
     );
 }
