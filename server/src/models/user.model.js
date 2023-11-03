3f310212c76293bc01561b66fb0764628e57976d
VangoCode <ronvarshavsky1@gmail.com>
2023-11-02 22:13:48 -0400

    TCES-12 refactor(auth) implement sequelize in separate file

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -1,27 +1,6 @@
 require("dotenv").config();
-
-const { Sequelize, DataTypes } = require("sequelize");
-const sequelize = new Sequelize(
-  process.env.DATABASE_NAME,
-  process.env.DATABASE_USERNAME,
-  process.env.DATABASE_PASSWORD,
-  {
-    host: process.env.DATABASE_HOST,
-    dialect: 'mysql',
-    dialectOptions: {
-      ssl: {
-        rejectUnauthorized: true
-      },
-    },
-    define: {
-        defaultScope: {
-            attributes: { exclude: ['createdAt', 'updatedAt'] }
-        },
-        timestamps: false,
-        freezeTableName: true
-    }
-  },
-);
+const { DataTypes } = require("sequelize");
+const { sequelize } = require('../configs/sequelize');
 
 
 const User = sequelize.define(process.env.USERS_TABLE, {
