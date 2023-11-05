4c29701d9dec8bbb3c020c5402b2b74e9abd0a8a
VangoCode <ronvarshavsky1@gmail.com>
2023-11-05 12:25:21 -0500

    TCES-12 refactor(auth_test) refactor test cases for auth to match formatter and linter

--- server/test/controllers/auth/createUser.test.js
+++ server/test/controllers/auth/createUser.test.js
@@ -1,28 +1,42 @@
 import { expect, vi, describe, it } from "vitest";
 import createUserRequestHandler from "../../../src/controllers/auth/createUser";
 
-describe('createUser test suite', () => {
-    it('should respond with a 403 if you are not logged in', async () => {
-        const mockReq = {  };
-        const mockRes = { 
-            status: (code) => { mockRes.statusCode = code; return { send: (message) => { return ; } } },
-            statusCode: 0
+describe("createUser test suite", () => {
+  it("should respond with a 403 if you are not logged in", async () => {
+    const mockReq = {};
+    const mockRes = {
+      status: (code) => {
+        mockRes.statusCode = code;
+        return {
+          send: (message) => {
+            return;
+          },
         };
-        const mockNext = vi.fn();
+      },
+      statusCode: 0,
+    };
+    const mockNext = vi.fn();
 
-        await createUserRequestHandler(mockReq, mockRes, mockNext);
-        expect(mockRes.statusCode).toBe(403);
-    });
+    await createUserRequestHandler(mockReq, mockRes, mockNext);
+    expect(mockRes.statusCode).toBe(403);
+  });
 
-    it('should respond with a 403 if you are not an admin', async () => {
-        const mockReq = { user: { is_admin: false } };
-        const mockRes = { 
-            status: (code) => { mockRes.statusCode = code; return { send: (message) => { return ; } } },
-            statusCode: 0
+  it("should respond with a 403 if you are not an admin", async () => {
+    const mockReq = { user: { is_admin: false } };
+    const mockRes = {
+      status: (code) => {
+        mockRes.statusCode = code;
+        return {
+          send: (message) => {
+            return;
+          },
         };
-        const mockNext = vi.fn();
+      },
+      statusCode: 0,
+    };
+    const mockNext = vi.fn();
 
-        await createUserRequestHandler(mockReq, mockRes, mockNext);
-        expect(mockRes.statusCode).toBe(403);
-    });
+    await createUserRequestHandler(mockReq, mockRes, mockNext);
+    expect(mockRes.statusCode).toBe(403);
+  });
 });
