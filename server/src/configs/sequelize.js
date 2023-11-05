dc9e79db96df7192a5878d5a42cb83d3f1e510e9
VangoCode <ronvarshavsky1@gmail.com>
2023-11-05 15:38:26 -0500

    TCES-12 hotfix(auth_test) fix sequelize connection failure to pass tests

--- server/src/configs/sequelize.js
+++ server/src/configs/sequelize.js
@@ -1,34 +1,34 @@
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(
-  process.env.DATABASE_NAME,
-  process.env.DATABASE_USERNAME,
-  process.env.DATABASE_PASSWORD,
-  {
-    host: process.env.DATABASE_HOST,
-    dialect: "mysql",
-    dialectOptions: {
-      ssl: {
-        rejectUnauthorized: true,
-      },
-    },
-    define: {
-      defaultScope: {
-        attributes: { exclude: ["createdAt", "updatedAt"] },
-      },
-      timestamps: false,
-      freezeTableName: true,
-    },
-  },
-);
-
 try {
+  const sequelize = new Sequelize(
+    process.env.DATABASE_NAME,
+    process.env.DATABASE_USERNAME,
+    process.env.DATABASE_PASSWORD,
+    {
+      host: process.env.DATABASE_HOST,
+      dialect: "mysql",
+      dialectOptions: {
+        ssl: {
+          rejectUnauthorized: true,
+        },
+      },
+      define: {
+        defaultScope: {
+          attributes: { exclude: ["createdAt", "updatedAt"] },
+        },
+        timestamps: false,
+        freezeTableName: true,
+      },
+    },
+  );
+
   sequelize.authenticate();
+
+  module.exports = {
+    sequelize,
+  };
 } catch {
   console.log("Sequelize could not connect");
 }
-
-module.exports = {
-  sequelize,
-};
