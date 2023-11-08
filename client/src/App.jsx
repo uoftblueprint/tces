a949a6d3818f9fa5b1ccca28f55cf6e107bd1b28
Taha <70107774+tsids@users.noreply.github.com>
2023-11-08 17:57:42 -0500

    TCES-41 urd create and update user information to be updated (#21)
    * Rendering Create and Edit User
    
    * Completing TCES-41 Added Create & Edit User pages
    
    * fixed linting errors
    
    * updated app.jsx
    
    * updated changes based on comments
    
    * fixed app
    
    * updated folder names
    
    * changed email and password input type
    
    * changed file names
    
    * added required attribute
    
    * changed onClick event to onSubmit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,8 @@
 import "./App.css";
 import DashboardPage from "./pages/dashboard";
 // import LoginPage from "./pages/login";
+// import CreatePage from "./pages/create-user/create-user";
+// import EditPage from "./pages/edit-user/edit-user";
 
 function App() {
   return (
