43065b14dcab5fbd2b38d5909af9334f904124f7
3m1ly03 <emilyfaithzhou@icloud.com>
2023-11-09 02:33:35 -0500

    TCES-28 Added job lead component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,18 @@
 import "./App.css";
 // import DashboardPage from "./pages/dashboard";
+// import DashboardPage from "./pages/dashboard";
 // import LoginPage from "./pages/login";
 // import CreatePage from "./pages/create-user/create-user";
 // import EditPage from "./pages/edit-user/edit-user";
 // import AddJobLead from "./components/add-job-lead-component";
 import AddCompanyInfo from "./components/add-employer-component/company-info-component";
+import AddJobLead from "./components/add-job-lead-component";
 
 function App() {
   return (
     <div className="App">
       <AddCompanyInfo />
+      <AddJobLead />
     </div>
   );
 }
