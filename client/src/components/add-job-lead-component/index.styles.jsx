9ba9e92c8f0d000cba130a0756aebaad33dc91c3
3m1ly03 <emilyfaithzhou@icloud.com>
2023-11-09 11:42:01 -0500

    TCES-28 Modified button dashes

--- client/src/components/add-job-lead-component/index.styles.jsx
+++ client/src/components/add-job-lead-component/index.styles.jsx
@@ -56,13 +56,14 @@ const Body = styled.body`
 const ButtonL = styled.button`
   width: 62%;
   margin: 10px;
-  padding: 12px;
-  font-size: 16px;
-  background-color: #4caf50;
+  padding: 16px;
+  font-size: 20px;
+  font-weight: 500;
   color: #3568e5;
   border-radius: 8px;
-  border: 2px dashed var(--light-primary-main, #3568e5);
+  border-width: 0px;
   background: var(--light-primary-shades-12-p, rgba(53, 104, 229, 0.12));
+  background-image: url("data:image/svg+xml,%3csvg width='100%25' height='100%25' xmlns='http://www.w3.org/2000/svg'%3e%3crect width='100%25' height='100%25' fill='none' rx='8' ry='8' stroke='%233568E5FF' stroke-width='4' stroke-dasharray='20%2c 10' stroke-dashoffset='2' stroke-linecap='butt'/%3e%3c/svg%3e");
 `;
 
 export { Container, ButtonContainer, JobLeadContainer, H3, H1, Body, ButtonL };
