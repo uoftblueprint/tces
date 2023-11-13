789608bd7f1726da37008904316c04bbdb043fbd
Jordan Janakievski <jajanak10@gmail.com>
2023-11-12 22:00:30 -0500

    Remove the extra tests

--- server/test/middleware/auth/isLoggedIn.test.js
+++ server/test/middleware/auth/isLoggedIn.test.js
@@ -20,31 +20,4 @@ describe("createUser test suite", () => {
     await isLoggedIn(mockReq, mockRes, mockNext);
     expect(mockRes.statusCode).toBe(403);
   });
-
-  it("should respond with 403 if the user is not in the database", async () => {
-    const mockReq = { user: { username: "user" } };
-    const mockRes = {
-      status: (code) => {
-        mockRes.statusCode = code;
-        return {
-          json: (message) => {
-            return;
-          },
-        };
-      },
-      statusCode: 0,
-    };
-    const mockNext = vi.fn();
-
-    vi.mock("../../../src/models/user.model", () => {
-      return {
-        findOne: async (options) => {
-          return null;
-        },
-      };
-    });
-
-    await isLoggedIn(mockReq, mockRes, mockNext);
-    expect(mockRes.statusCode).toBe(403);
-  });
 });
