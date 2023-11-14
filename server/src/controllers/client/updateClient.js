a2d03eb21197118d74f024bb2e2cf5138bedb14d
VangoCode <ronvarshavsky1@gmail.com>
2023-11-14 18:57:52 -0500

    TCES-13 feat(updateClient) implements updating a client feature

---
+++ server/src/controllers/client/updateClient.js
@@ -0,0 +1,37 @@
+const logger = require("pino")();
+const Client = require("../../models/client.model");
+
+const updateClientRequestHandler = async (req, res) => {
+  try {
+    const client_id = req.params.client_id;
+    const client = await Client.findOne({where: {id: client_id}})
+    if (!client) {
+      return res.status(404).json({status: "fail", message: "User not found", data: null});
+    }
+    if (!req.body.values) {
+      return res.status(401).json({status: "fail", message: "Missing values to update in parameters", data: null});
+    }
+
+    await client.set(req.body.values);
+    await client.save();
+
+    return res.status(200).json({status: "success", message: "Client updated successfully", data: client});
+
+  } catch(err) {
+    res.json(err);
+    if (err.name == "SequelizeUniqueConstraintError") {
+      // This means that either user or owner is not a valid user
+      return res.status(400).json({
+        status: "fail",
+        message:
+          "Either owner or creator is not a valid user, OR the email is already in use",
+        data: null,
+      });
+    }
+
+    logger.error(`Unexpected server error: ${err}`);
+    return res.status(500).json({status: "error", message: "Unexpected server error"});
+  }
+};
+
+module.exports = updateClientRequestHandler;
--- server/src/routes/client.js
+++ server/src/routes/client.js
@@ -3,11 +3,13 @@ const isLoggedIn = require("../middlewares/auth/isLoggedIn");
 const getOneClientRequestHandler = require("../controllers/client/getOneClient");
 const getAllClientsRequestHandler = require("../controllers/client/getAllClients");
 const addClientsRequestHandler = require("../controllers/client/addClients");
+const updateClientRequestHandler = require("../controllers/client/updateClient");
 
 const router = express.Router();
 
 router.get("/:client_id", isLoggedIn, getOneClientRequestHandler);
 router.get("/", isLoggedIn, getAllClientsRequestHandler);
 router.post("/", isLoggedIn, addClientsRequestHandler);
+router.put("/:client_id", isLoggedIn, updateClientRequestHandler);
 
 module.exports = router;
