51d8cceed19ad2cbf03e57bed00dbb0500e82247
VangoCode <ronvarshavsky1@gmail.com>
2023-11-14 18:28:52 -0500

    TCES-13 feat(getOneClient.test.js) add mocking for getting one client

--- server/test/controllers/getOneClient.test.js
+++ server/test/controllers/getOneClient.test.js
@@ -1,17 +1,23 @@
 import { expect, vi, describe, it, afterEach } from "vitest";
-import addClientsRequestHandler from "../../src/controllers/client/addClients";
+import getOneClientRequestHandler from "../../src/controllers/client/getOneClient";
 const Client = await require("../../src/models/client.model");
 
 vi.hoisted(() => {
   const mock = require("mock-require");
 
   mock("../../src/models/client.model", {
-    findOne: vi.fn(),
-    findAll: vi.fn(),
+    findOne: () => {return {}},
+    findAll: () => {return {}},
   });
 });
 
-describe("addClients test suite", () => {
+describe("getOneClient test suite", () => {
+  const mockReq = {
+    params: {
+      client_id: 1,
+    },
+  };
+  
   var mockRes = {
     status: (code) => {
       mockRes.statusCode = code;
@@ -29,101 +35,22 @@ describe("addClients test suite", () => {
     mockRes.statusCode = 0;
   });
 
-  describe("Add single client", () => {
-    var mockReq = {
-      body: {
-        client: {
-          owner: 1,
-          creator: 1,
-          name: "name",
-          email: "email@gmail.com",
-          phone_number: "289-555-5555",
-          status: "open?",
-          closure_date: new Date(),
-          status_at_exit: "active",
-          status_at_3_months: "active",
-          status_at_6_months: "active",
-          status_at_12_months: "active",
-        },
-      },
-    };
+  it("Calls findOne", async () => {
+    const spy = vi.spyOn(Client, "findOne");
 
-    it("Calls create", async () => {
-      const spy = vi.spyOn(Client, "create");
-
-      await addClientsRequestHandler(mockReq, mockRes);
-      expect(spy).toHaveBeenCalledTimes(1);
-    });
-
-    it("Does not call bulkCreate", async () => {
-      const spy = vi.spyOn(Client, "bulkCreate");
-
-      await addClientsRequestHandler(mockReq, mockRes);
-      expect(spy).toHaveBeenCalledTimes(0);
-    });
-
-    it("Returns 200 on success", async () => {
-      await addClientsRequestHandler(mockReq, mockRes);
-      expect(mockRes.statusCode).toBe(200);
-    });
+    await getOneClientRequestHandler(mockReq, mockRes);
+    expect(spy).toHaveBeenCalledTimes(1);
   });
 
-  describe("Add multiple clients", () => {
-    const mockReq = {
-      body: {
-        client: [
-            {
-              owner: 1,
-              creator: 1,
-              name: "name",
-              email: "email@gmail.com",
-              phone_number: "289-555-5555",
-              status: "open?",
-              closure_date: new Date(),
-              status_at_exit: "active",
-              status_at_3_months: "active",
-              status_at_6_months: "active",
-              status_at_12_months: "active",
-            },
-            {
-              owner: 1,
-              creator: 2,
-              name: "name",
-              email: "email2@gmail.com",
-              phone_number: "289-555-5555",
-              status: "open?",
-              closure_date: new Date(),
-              status_at_exit: "active",
-              status_at_3_months: "active",
-              status_at_6_months: "active",
-              status_at_12_months: "active",
-            }
-          ],
-      },
-    };
-    
-    it("Calls bulkCreate", async () => {
+  it("Does not call findAll", async () => {
+    const spy = vi.spyOn(Client, "findAll");
 
+    await getOneClientRequestHandler(mockReq, mockRes);
+    expect(spy).toHaveBeenCalledTimes(0);
+  });
 
-      const spy = vi.spyOn(Client, "bulkCreate");
-  
-      await addClientsRequestHandler(mockReq, mockRes);
-      expect(spy).toHaveBeenCalledTimes(1);
-    });
-
-    it("Does not call create", async () => {
-
-
-      const spy = vi.spyOn(Client, "create");
-  
-      await addClientsRequestHandler(mockReq, mockRes);
-      expect(spy).toHaveBeenCalledTimes(0);
-    });
-
-    it("Returns 200 on success", async () => {
-      await addClientsRequestHandler(mockReq, mockRes);
-      expect(mockRes.statusCode).toBe(200);
-    });
-
+  it("Returns 200 on success", async () => {
+    await getOneClientRequestHandler(mockReq, mockRes);
+    expect(mockRes.statusCode).toBe(200);
   });
 });
