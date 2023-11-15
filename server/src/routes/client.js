dde7ccf5d0ddfd2baee1f4394b99e902f5a1a824
VangoCode <ronvarshavsky1@gmail.com>
2023-11-14 21:43:05 -0500

    TCES-13 refactor(client) add parameter comments

--- server/src/routes/client.js
+++ server/src/routes/client.js
@@ -7,9 +7,44 @@ const updateClientRequestHandler = require("../controllers/client/updateClient")
 
 const router = express.Router();
 
+/**
+ * Get a specific client's info, with id client_id
+ *
+ * Expected parameters:
+ * @type integer (in url) {params.client_id}
+ */
 router.get("/:client_id", isLoggedIn, getOneClientRequestHandler);
+
+/**
+ * Get all client's info
+ *
+ * Expected parameters:
+ */
 router.get("/", isLoggedIn, getAllClientsRequestHandler);
+
+/**
+ * Create a new client
+ *
+ * Expected body parameters:
+ * @type Client || Client[] {params.body.client}
+ *   @type integer {params.body.client.owner}
+ *   @type integer {params.body.client.creator}
+ *   @type string {params.body.client.name}
+ *   @type string {params.body.client.email}
+ *   @type string {params.body.client.phone_number}
+ *   @type string {params.body.client.status}
+ *   @type string {params.body.client.closure_date}
+ */
 router.post("/", isLoggedIn, addClientsRequestHandler);
+
+/**
+ * Update a specific client's info, with id client_id
+ *
+ * Expected parameters:
+ * @type string {params.client_id}
+ * Expected body parameters:
+ * @type Client {params.body.client}
+ */
 router.put("/:client_id", isLoggedIn, updateClientRequestHandler);
 
 module.exports = router;
