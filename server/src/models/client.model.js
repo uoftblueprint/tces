5f528856551410882b766fd4e96bf2aabc9c31f2
VangoCode <ronvarshavsky1@gmail.com>
2023-11-16 21:54:02 -0500

    TCES-13 update(client) update client.model to match new changes

--- server/src/models/client.model.js
+++ server/src/models/client.model.js
@@ -52,7 +52,10 @@ const Client = sequelize.define("clients", {
     allowNull: true,
   },
   status: {
-    type: DataTypes.STRING,
+    type: DataTypes.ENUM([
+      "active",
+      "closed",
+    ]),
     allowNull: true,
   },
   closure_date: {
@@ -62,37 +65,33 @@ const Client = sequelize.define("clients", {
   },
   status_at_exit: {
     type: DataTypes.ENUM([
-      "active",
-      "closed_to_employed",
-      "closed_to_training",
-      "closed_to_no_results",
+      "employed",
+      "training",
+      "no_results",
     ]),
     allowNull: true,
   },
   status_at_3_months: {
     type: DataTypes.ENUM([
-      "active",
-      "closed_to_employed",
-      "closed_to_training",
-      "closed_to_no_results",
+      "employed",
+      "training",
+      "no_results",
     ]),
     allowNull: true,
   },
   status_at_6_months: {
     type: DataTypes.ENUM([
-      "active",
-      "closed_to_employed",
-      "closed_to_training",
-      "closed_to_no_results",
+      "employed",
+      "training",
+      "no_results",
     ]),
     allowNull: true,
   },
   status_at_12_months: {
     type: DataTypes.ENUM([
-      "active",
-      "closed_to_employed",
-      "closed_to_training",
-      "closed_to_no_results",
+      "employed",
+      "training",
+      "no_results",
     ]),
     allowNull: true,
   },
