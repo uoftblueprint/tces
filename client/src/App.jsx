39530469fded8bd15d93573ca62e0ba4075982b9
3m1ly03 <emilyfaithzhou@icloud.com>
2023-11-19 17:45:13 -0500

    TCES-28 Fixed spacing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,10 +4,12 @@ import "./App.css";
 // import CreatePage from "./pages/create-user/create-user";
 // import EditPage from "./pages/edit-user/edit-user";
 import AddJobLead from "./components/add-job-lead-component";
+import Navbar from "./components/navbar-component/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <AddJobLead />
     </div>
   );
