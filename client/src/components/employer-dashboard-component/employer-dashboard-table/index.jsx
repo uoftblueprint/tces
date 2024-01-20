4f49157f13bdef71aa143ce2fe5c2aee4b7befc4
3m1ly03 <emilyfaithzhou@icloud.com>
2024-01-20 01:44:03 -0500

    Switched to data grid

--- client/src/components/employer-dashboard-component/employer-dashboard-table/index.jsx
+++ client/src/components/employer-dashboard-component/employer-dashboard-table/index.jsx
@@ -1,16 +1,6 @@
 import PropTypes from "prop-types";
-import {
-  TablePagination,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  Paper,
-  Chip,
-  Avatar,
-} from "@mui/material";
+import { Chip, Avatar } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
 
 function EmployerDashboardTable({
   slicedData,
@@ -23,65 +13,64 @@ function EmployerDashboardTable({
   const formatDate = (date) => {
     return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
   };
+
+  const columns = [
+    { field: "employerName", headerName: "Employer Name", width: 200 },
+    {
+      field: "date",
+      headerName: "Date",
+      width: 200,
+      valueFormatter: ({ value }) => formatDate(value),
+    },
+    { field: "phoneNumber", headerName: "Phone Number", width: 200 },
+    { field: "email", headerName: "Email", width: 200 },
+    { field: "primaryContact", headerName: "Primary Contact", width: 200 },
+    {
+      field: "owner",
+      headerName: "Owner",
+      width: 150,
+      renderCell: (params) => (
+        <Chip
+          avatar={<Avatar>{/* logic for the avatar */}</Avatar>}
+          label={params.value}
+        />
+      ),
+    },
+  ];
+
+  const rows = slicedData.map((item, index) => ({
+    id: index,
+    employerName: item.employerName,
+    date: item.date,
+    phoneNumber: item.phoneNumber,
+    email: item.email,
+    primaryContact: item.primaryContact,
+    owner: item.owner,
+  }));
+
   return (
-    <TableContainer
-      component={Paper}
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "space-between",
-        height: "fit-content",
-      }}
-    >
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell align="left">Employer Name</TableCell>
-            <TableCell align="left">Date</TableCell>
-            <TableCell align="left">Phone Number</TableCell>
-            <TableCell align="left">Email</TableCell>
-            <TableCell align="left">Primary Contact</TableCell>
-            <TableCell align="left">Owner</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody sx={{ overflowY: "auto" }}>
-          {slicedData.map(
-            (
-              { employerName, date, phoneNumber, email, primaryContact, owner },
-              index,
-            ) => (
-              <TableRow
-                // eslint-disable-next-line react/no-array-index-key
-                key={`${employerName}-${index}`}
-                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-              >
-                <TableCell align="left">{employerName}</TableCell>
-                <TableCell align="left">{formatDate(date)}</TableCell>
-                <TableCell align="left">{phoneNumber}</TableCell>
-                <TableCell align="left">{email}</TableCell>
-                <TableCell align="left">{primaryContact}</TableCell>
-                <TableCell align="left">
-                  <Chip avatar={<Avatar>{/* photo */}</Avatar>} label={owner} />
-                </TableCell>
-              </TableRow>
-            ),
-          )}
-        </TableBody>
-      </Table>
-      <TablePagination
-        component="div"
-        count={count}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onPageChange={handleChangePage}
-        onRowsPerPageChange={handleChangeRowsPerPage}
+    <div style={{ minHeight: 500, minWidth: "80%" }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        pageSize={rowsPerPage}
+        page={page - 1}
+        onPageSizeChange={(newPageSize) =>
+          handleChangeRowsPerPage({ target: { value: newPageSize } })
+        }
+        onPageChange={(newPage) => handleChangePage(null, newPage)}
+        rowCount={count}
+        pagination
+        paginationMode="server"
+        sx={{
+          background: "white",
+        }}
       />
-    </TableContainer>
+    </div>
   );
 }
 
 EmployerDashboardTable.propTypes = {
-  // eslint-disable-next-line react/forbid-prop-types
   slicedData: PropTypes.arrayOf(PropTypes.object).isRequired,
   page: PropTypes.number.isRequired,
   rowsPerPage: PropTypes.number.isRequired,
--- client/src/components/employer-dashboard-component/index.styles.jsx
+++ client/src/components/employer-dashboard-component/index.styles.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 const Container = styled.div`
   background-color: #fafafa;
+  min-height: 100vh;
 `;
 
 const DashboardContainer = styled.div`
