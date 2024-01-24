2b3d3392679c5e6523643ec3b1ce43e2d70ded3b
VangoCode <ronvarshavsky1@gmail.com>
2024-01-23 19:12:27 -0500

    TCES-23 feat(employer) extract job leads into separate component

--- client/src/components/employer-component/index.jsx
+++ client/src/components/employer-component/index.jsx
@@ -1,17 +1,10 @@
 // import * as React from "react";
 
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
-import SearchIcon from "@mui/icons-material/Search";
-import AddIcon from "@mui/icons-material/Add";
-import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
-import TextField from "@mui/material/TextField";
-import InputAdornment from "@mui/material/InputAdornment";
 import { useNavigate, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 
@@ -30,6 +23,7 @@ import {
 import EmployerInfoComponent from "./employer-info-component";
 import EmployerInformationCard from "./employer-information-card";
 import ContactsInformationCard from "./contacts-information-card";
+import EmployerJobLeadsCard from "./employer-job-leads-card";
 
 function EmployerComponent({ getUserById, managedUsers, setSnackBarMessage }) {
   const navigate = useNavigate();
@@ -125,68 +119,7 @@ function EmployerComponent({ getUserById, managedUsers, setSnackBarMessage }) {
               </Card>
             </Box>
 
-            <Box
-              sx={{
-                display: "flex",
-                flexDirection: "row",
-                gridColumnGap: "30px",
-                height: 400,
-                marginTop: "10px",
-                width: "100%",
-              }}
-            >
-              <Card
-                style={{
-                  width: "66%",
-                }}
-              >
-                <CardContent
-                  style={{
-                    display: "flex",
-                    justifyContent: "space-between",
-                  }}
-                >
-                  <Typography variant="h5" align="left" gutterBottom>
-                    Job Leads
-                  </Typography>
-                  <IconButton>
-                    <AddIcon
-                      sx={{
-                        color: "gray",
-                        cursor: "pointer",
-                        align: "center",
-                      }}
-                    />
-                  </IconButton>
-                </CardContent>
-                <Divider />
-                <CardContent>
-                  <Typography sx={{ mb: 0.5 }} color="text.secondary">
-                    <TextField
-                      type="text"
-                      value="aaa"
-                      onChange={() => {}}
-                      size="small"
-                      style={{
-                        borderWidth: "10px",
-                      }}
-                      InputProps={{
-                        endAdornment: (
-                          <InputAdornment position="end">
-                            <SearchIcon />
-                          </InputAdornment>
-                        ),
-                      }}
-                    />
-                  </Typography>
-                </CardContent>
-                <CardActions>
-                  <Button size="small" onClick={() => {}}>
-                    Reset Filters
-                  </Button>
-                </CardActions>
-              </Card>
-            </Box>
+            <EmployerJobLeadsCard />
           </EmployerContainer>
         </MainContainer>
       </div>
