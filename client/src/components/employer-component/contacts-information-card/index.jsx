75803f2ef7fed994caa9c6a67b4e2f45cf007414
VangoCode <ronvarshavsky1@gmail.com>
2024-01-23 19:30:25 -0500

    TCES-23 feat(employer) implemented employer contact message when no contacts

--- client/src/components/employer-component/contacts-information-card/index.jsx
+++ client/src/components/employer-component/contacts-information-card/index.jsx
@@ -1,6 +1,7 @@
 import EditIcon from "@mui/icons-material/Edit";
 import IconButton from "@mui/material/IconButton";
 import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
 import AddIcon from "@mui/icons-material/Add";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
@@ -67,75 +68,84 @@ function ContactsInformationCard({ contacts }) {
         </div>
       </CardContent>
       <Divider />
-      {/* we will be setting up custom pagination for this */}
-      <CardContent>
-        <BoxRowComponent
-          leftSide="Name"
-          rightSide={contacts[0] ? contacts[contactPage - 1].name : ""}
-        />
-        <BoxRowComponent
-          leftSide="Job Title"
-          rightSide={contacts[0] ? contacts[contactPage - 1].jobTitle : ""}
-        />
-        <BoxRowComponent
-          leftSide="Email"
-          rightSide={
-            <a
-              href={`mailto:${contacts[0] ? contacts[contactPage - 1].email : ""}`}
-            >{`${contacts[0] ? contacts[contactPage - 1].email : ""}`}</a>
-          }
-          copyable
-        />
-        <BoxRowComponent
-          leftSide="Phone Number"
-          rightSide={
-            <a
-              href={`tel:${contacts[0] ? contacts[contactPage - 1].phoneNumber : ""}`}
-            >
-              {contacts[0] ? contacts[contactPage - 1].phoneNumber : ""}
-            </a>
-          }
-        />
-        <BoxRowComponent
-          leftSide="Alternative Phone Number"
-          rightSide={
-            <a
-              href={`tel:${contacts[0] ? contacts[contactPage - 1].alternatePhoneNumber : ""}`}
-            >
-              {contacts[0] ? contacts[contactPage - 1].phoneNumber : ""}
-            </a>
-          }
-        />
-        <Grid
-          container
-          style={{
-            display: "flex",
-            justifyContent: "flex-end",
-          }}
-        >
-          <div className="contactPageNumber" style={{ padding: 8 }}>
-            {contactPage} of {contacts.length}
-          </div>
-          <IconButton onClick={leftButtonClick}>
-            <ChevronLeftIcon
-              sx={{
-                color: "gray",
-                cursor: "pointer",
-                align: "center",
-              }}
-            />
-          </IconButton>
-          <IconButton onClick={rightButtonClick}>
-            <ChevronRightIcon
-              sx={{
-                color: "gray",
-                cursor: "pointer",
-                align: "center",
-              }}
-            />
-          </IconButton>
-        </Grid>
-      </CardContent>
+      {contacts.length > 0 ? (
+        <CardContent>
+          <BoxRowComponent
+            leftSide="Name"
+            rightSide={contacts[0] ? contacts[contactPage - 1].name : ""}
+          />
+          <BoxRowComponent
+            leftSide="Job Title"
+            rightSide={contacts[0] ? contacts[contactPage - 1].jobTitle : ""}
+          />
+          <BoxRowComponent
+            leftSide="Email"
+            rightSide={
+              <a
+                href={`mailto:${contacts[0] ? contacts[contactPage - 1].email : ""}`}
+              >{`${contacts[0] ? contacts[contactPage - 1].email : ""}`}</a>
+            }
+            copyable
+          />
+          <BoxRowComponent
+            leftSide="Phone Number"
+            rightSide={
+              <a
+                href={`tel:${contacts[0] ? contacts[contactPage - 1].phoneNumber : ""}`}
+              >
+                {contacts[0] ? contacts[contactPage - 1].phoneNumber : ""}
+              </a>
+            }
+          />
+          <BoxRowComponent
+            leftSide="Alternative Phone Number"
+            rightSide={
+              <a
+                href={`tel:${contacts[0] ? contacts[contactPage - 1].alternatePhoneNumber : ""}`}
+              >
+                {contacts[0] ? contacts[contactPage - 1].phoneNumber : ""}
+              </a>
+            }
+          />
+          <Grid
+            container
+            style={{
+              display: "flex",
+              justifyContent: "flex-end",
+            }}
+          >
+            <div className="contactPageNumber" style={{ padding: 8 }}>
+              {contactPage} of {contacts.length}
+            </div>
+            <IconButton onClick={leftButtonClick}>
+              <ChevronLeftIcon
+                sx={{
+                  color: "gray",
+                  cursor: "pointer",
+                  align: "center",
+                }}
+              />
+            </IconButton>
+            <IconButton onClick={rightButtonClick}>
+              <ChevronRightIcon
+                sx={{
+                  color: "gray",
+                  cursor: "pointer",
+                  align: "center",
+                }}
+              />
+            </IconButton>
+          </Grid>
+        </CardContent>
+      ) : (
+        <Box textAlign="center" sx={{ mt: 4 }}>
+          <img
+            src="/noEmployerContacts.svg"
+            alt="No Employer Contacts"
+            style={{ width: "400px", marginTop: "20px" }}
+          />
+        </Box>
+      )}
     </Card>
   );
 }
--- client/src/mock-data/mockEmployerContacts.jsx
+++ client/src/mock-data/mockEmployerContacts.jsx
@@ -1,17 +1,17 @@
 const mockEmployerContacts = [
-  {
-    name: "Ron Var",
-    jobTitle: "Student",
-    email: "ron@ronvar.ca",
-    phoneNumber: "1 416 967 1111",
-  },
-  {
-    name: "Coriolanus Snow",
-    jobTitle: "President",
-    email: "snow@ilovelucygray.capitol",
-    phoneNumber: "1 416 967 1111",
-    alternatePhoneNumber: "1 416 555 5555",
-  },
+  // {
+  //   name: "Ron Var",
+  //   jobTitle: "Student",
+  //   email: "ron@ronvar.ca",
+  //   phoneNumber: "1 416 967 1111",
+  // },
+  // {
+  //   name: "Coriolanus Snow",
+  //   jobTitle: "President",
+  //   email: "snow@ilovelucygray.capitol",
+  //   phoneNumber: "1 416 967 1111",
+  //   alternatePhoneNumber: "1 416 555 5555",
+  // },
 ];
 
 export default mockEmployerContacts;
