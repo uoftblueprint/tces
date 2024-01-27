513caa2a67882e0260dc6267ba2850119d9497b0
3m1ly03 <emilyfaithzhou@icloud.com>
2024-01-26 19:39:35 -0500

    Added general entry template

---
+++ client/src/components/timeline-entry-components/card-template-component/index.jsx
@@ -0,0 +1,39 @@
+import PropTypes from "prop-types";
+import {
+  Card, 
+  ImageContainer, 
+  Image,
+  Line,
+  ContentContainer, 
+  Header, 
+  Title, 
+  DateText, 
+  Body
+} from "./index.styles";
+
+function CardComponent({ title, dateAdded, body, imageUrl }) {
+    return (
+      <Card>
+        <ImageContainer>
+          <Image src={imageUrl} alt="Card Image" />
+          <Line />
+        </ImageContainer>
+        <ContentContainer>
+          <Header>
+            <Title>{title}</Title>
+            <DateText>{new Date(dateAdded).toLocaleDateString()}</DateText>
+          </Header>
+          <Body>{body}</Body>
+        </ContentContainer>
+      </Card>
+    );
+}
+
+CardComponent.propTypes = {
+  title: PropTypes.string.isRequired,
+  dateAdded: PropTypes.string.isRequired,
+  body: PropTypes.string.isRequired,
+  imageUrl: PropTypes.string.isRequired,
+};
+  
+export default CardComponent;
---
+++ client/src/components/timeline-entry-components/card-template-component/index.styles.jsx
@@ -0,0 +1,67 @@
+import styled from 'styled-components';
+
+const Card = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 400px;
+  height: fit-content;
+  align-items: stretch; // Change from flex-start to stretch to fill the height
+  border: 1px solid #0000001F;
+  padding: 16px;
+  background: #FFF;
+`;
+
+const ImageContainer = styled.div`
+  display: flex;
+  flex-grow: 1;
+  flex-direction: column;
+  align-items: center;
+  margin-right: 10px;
+`;
+
+const Image = styled.img`
+  width: 25px;
+  height: 25px;
+`;
+
+const Line = styled.div`
+  width: 1px;
+  height: 100%;
+  background-color: #0000001F;
+`;
+
+const ContentContainer = styled.div`
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
+
+const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+`;
+
+const Title = styled.h4`
+  margin: 0;
+  font-family: Inter;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 500;
+`;
+
+const DateText = styled.div`
+  color: var(--Light-Text-Secondary, rgba(0, 0, 0, 0.60));
+  font-size: 6px;
+`;
+
+const Body = styled.p`
+  font-family: Inter;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 400;
+  margin-top: 8px;
+`;
+
+export {Card, ImageContainer, Image, Line, ContentContainer, Header, Title, DateText, Body};
