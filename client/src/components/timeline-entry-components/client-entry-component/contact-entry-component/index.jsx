5179ab22b81494320295c9d64949c2a379ef20a5
3m1ly03 <emilyfaithzhou@icloud.com>
2024-01-26 19:55:53 -0500

    Created client entries

---
+++ client/src/components/timeline-entry-components/client-entry-component/contact-entry-component/index.jsx
@@ -0,0 +1,28 @@
+import PropTypes from "prop-types";
+import CardComponent from "../../card-template-component";
+
+function ClientContactComponent ({ ...props }) {
+    return (
+       <CardComponent title={props.title} dateAdded={props.dateAdded} body={props.body} imageUrl={"/img/timelineIconContact.svg"} />
+    );
+};
+
+ClientContactComponent.propTypes = {
+    entry: PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        dateAdded: PropTypes.string.isRequired,
+        type: PropTypes.oneOf(['update', 'contact', 'placement', 'note']).isRequired,
+        title: PropTypes.string.isRequired,
+        body: PropTypes.string.isRequired,
+        client: PropTypes.shape({
+            clientId: PropTypes.number,
+            name: PropTypes.string
+        }),
+        // eslint-disable-next-line react/forbid-prop-types
+        jobLead: PropTypes.object, // not sure how to define this
+        // eslint-disable-next-line react/forbid-prop-types
+        employerContact: PropTypes.object // same as above
+    }).isRequired
+};
+
+export default ClientContactComponent;
---
+++ client/src/components/timeline-entry-components/client-entry-component/index.jsx
@@ -0,0 +1,56 @@
+import PropTypes from "prop-types";
+import ClientUpdateComponent from "./update-entry-component";
+import ClientContactComponent from "./contact-entry-component";
+import ClientPlacementComponent from "./placement-entry-component";
+import ClientNoteComponent from "./note-entry-component";
+
+function ClientEntryComponent ({ entry }) {
+    const { type, ...otherProps } = entry;
+
+    const renderComponent = () => {
+        switch (entry.type) {
+            case 'update':
+                return <ClientUpdateComponent {...otherProps} />;
+            case 'contact':
+                return <ClientContactComponent {...otherProps} />;
+            case 'placement':
+                return <ClientPlacementComponent {...otherProps} />;
+            case 'note':
+                return <ClientNoteComponent {...otherProps} />;
+            default:
+                return <div>Unknown Entry Type</div>;
+        }
+    };
+
+    return (
+        <div style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100vh'
+        }}>
+            {renderComponent()}
+        </div>
+        
+    );
+};
+
+ClientEntryComponent.propTypes = {
+    entry: PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        dateAdded: PropTypes.string.isRequired,
+        type: PropTypes.oneOf(['update', 'contact', 'placement', 'note']).isRequired,
+        title: PropTypes.string.isRequired,
+        body: PropTypes.string.isRequired,
+        client: PropTypes.shape({
+            clientId: PropTypes.number,
+            name: PropTypes.string
+        }),
+        // eslint-disable-next-line react/forbid-prop-types
+        jobLead: PropTypes.object, // not sure how to define this
+        // eslint-disable-next-line react/forbid-prop-types
+        employerContact: PropTypes.object // same as above
+    }).isRequired
+};
+
+export default ClientEntryComponent;
