4a084ed0af208403a6b9c6da70ce75efd43a7f8d
Selin Naz Taşman <68869318+selintasman@users.noreply.github.com>
2024-01-29 15:44:00 -0500

    TCES-22 CLT-Client Page (#37)
    * Completed the initial page
    
    * Completed the edit page
    
    * Trying to fix dependecy issues
    
    * Got imask working
    
    * Fixed the divider problem
    
    * Added the respective edit-client and client pages
    
    * Changed Table to Grid
    
    * Fixed the styling issues and addressed some  comments
    
    * Deleted unused code
    
    * Made all the necessary changes
    
    * Deleted unused code & made one button clickable
    
    * Added the copyIcon
    
    * Fixed the spacing issue for client page
    
    * Implemented the fixes from the standup
    
    * Went over the last comments
    
    * Removed the route for edit
    
    * Added Kevin's owner/creator card
    
    * Changed the owner card to match main
    
    * Moved some of the router code
    
    * Added the changeOwnerDialog
    
    * Fixed styling + status
    
    * Removed the icon for the status chip
    
    ---------
    
    Co-authored-by: Daniel Dervishi <d.dervishi@mail.utoronto.ca>

---
+++ client/src/pages/client-page/index.jsx
@@ -0,0 +1,73 @@
+import * as React from "react";
+
+import Client from "../../components/client-page";
+// import { getUserByIdHelper } from "../../utils/users";
+
+
+// const [managedUsers, setManagedUsers] = React.useState([]);
+
+
+function ClientPage() {
+
+  const today = new Date();
+  today.setMonth(today.getMonth() - 3);
+  const threeMonthsAgo = today.toLocaleDateString("en-US");
+  const closureDate = new Date(threeMonthsAgo);
+  const currentDate = new Date();
+  const timeDifference = currentDate - closureDate;
+  const monthsSinceClosure = Math.floor(
+    timeDifference / (1000 * 60 * 60 * 24 * 30.44),
+  );
+
+  const dummyOwner = {
+    userID: 1,
+    firstName: 'John',
+    lastName: 'Doe',
+    email: 'john.doe@example.com',
+    isAdmin: true,
+  };
+
+  const dummyCreator = {
+    userID: 2,
+    firstName: 'Selin',
+    lastName: 'Tasman',
+    email: 'selin.doe@example.com',
+    isAdmin: true,
+  };
+
+  const initialClientInfo = {
+    firstName: "First Name",
+    email: "email@email.com",
+    phone: "+1 111 111 1111",
+    status: "Closed",
+    closure_date: threeMonthsAgo,
+    time_since_closure: `${monthsSinceClosure} Months`,
+    status_at_exit: "Employed",
+    status_at_3: "",
+    status_at_6: "Employed",
+    status_at_9: "Employed",
+    status_at_12: "Training",
+    owner: dummyOwner,
+    creator: dummyCreator,
+  };
+
+  const [clientInfo, setClientInfo] = React.useState(initialClientInfo);
+  const { owner, creator } = clientInfo;
+
+
+  const handleSave = (updatedClientInfo) => {
+    setClientInfo(updatedClientInfo);
+  };
+
+  return (
+    <Client
+      clientInfo={clientInfo}
+      monthsSinceClosure={monthsSinceClosure}
+      onSaveChanges={handleSave}
+      owner={owner}
+      creator={creator}
+    />
+  );
+}
+
+export default ClientPage;
