ef5e28f7ec531437b1ad86d7596d626823c4fdf9
Selin Naz Tasman <selin.tasman@mail.utoronto.ca>
2024-01-29 01:09:38 -0500

    Partiially completed the main frame

--- client/src/components/timeline-window-component/index.jsx
+++ client/src/components/timeline-window-component/index.jsx
@@ -1,21 +1,28 @@
-import Button from "@mui/material/Button";
+import * as React from "react";
 import {
-    // TextField,
-    // Stack,
-    // Button,
+    Button,
+    ButtonGroup,
     Grid,
     Card,
     CardContent,
-    Box
-    // Typography,
+    Box,
+    Tab,
+    Typography,
+    Divider,
   } from "@mui/material";
   import MoreVertIcon from '@mui/icons-material/MoreVert';
-  import ButtonGroup from "@mui/material/ButtonGroup";
   import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
-  import Typography from "@mui/material/Typography";
+  import TabContext from '@mui/lab/TabContext';
+  import TabList from '@mui/lab/TabList';
+  import TabPanel from '@mui/lab/TabPanel';
 
 
 export default function Timeline() {
+  const [value, setValue] = React.useState('1');
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
     
     return (
     <Card style={{ width: "400px"}}>
@@ -34,17 +41,38 @@ export default function Timeline() {
           <Box>
             <ButtonGroup
               variant="contained"
-              style={{ width: "100%" }}
+              style={{ width: "100%", borderRadius: "8px" }}
             >
-              <Button style={{ width: "100%" }}> + Add new Entry</Button>
+              <Button style={{ width: "100%", backgroundColor: "#3568E5" }}> + Add new Entry</Button>
               <Button
                 size="small"
                 aria-label="select merge strategy"
                 aria-haspopup="menu"
+                style={{paddingTop: "6px", 
+                paddingRight:"16px", 
+                paddingBottom: "6px", 
+                paddingLeft: "16px",
+                backgroundColor: "#3568E5"
+              }}
               >
                 <ArrowDropDownIcon />
               </Button>
             </ButtonGroup>
+            <TabContext value={value}>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <TabList onChange={handleChange} aria-label="lab API tabs example">
+            <Tab label="Item One" value="1" />
+            <Tab label="Item Two" value="2" />
+            <Tab label="Item Three" value="3" />
+          </TabList>
+        </Box>
+        <Divider variant="middle"/>
+        <TabPanel value="1">Item One</TabPanel>
+        <TabPanel value="2">Item Two</TabPanel>
+        <TabPanel value="3">Item Three</TabPanel>
+      </TabContext>
+      
+      
           </Box>
         </CardContent>             
     </Card>
