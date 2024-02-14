1c6ba81d0462d3ada040e6f6cc93721711392a4c
Kevin Le <74465053+kevinle623@users.noreply.github.com>
2024-02-13 19:17:38 -0500

    fixed styling for the apply filter and reset button

--- client/src/components/client-dashboard-component/client-dashboard-filter/FilterCard.jsx
+++ client/src/components/client-dashboard-component/client-dashboard-filter/FilterCard.jsx
@@ -275,18 +275,16 @@ function FilterCard({
           </FormControl>
         </Stack>
       </CardContent>
-      <CardActions sx={{ justifyContent: "center", p: 2 }}>
+      <CardActions sx={{ display: "flex", p: 2, flexDirection: "column" }}>
         <Button
-          size="small"
-          onClick={onFilterReset}
-          color="warning"
-          variant="outlined"
-          sx={{ mr: 1 }}
+          onClick={onApplyFilterClick}
+          variant="contained"
+          sx={{ width: "100%" }}
         >
-          Reset
+          APPLY FILTER
         </Button>
-        <Button size="small" onClick={onApplyFilterClick} variant="contained">
-          Apply
+        <Button onClick={onFilterReset} sx={{ mt: 2, alignSelf: "flex-start" }}>
+          RESET FILTERS
         </Button>
       </CardActions>
     </Card>
