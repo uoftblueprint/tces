2951e96bf7bb0bd94d71028cfcce9edec51fb5c5
Kevin Le <74465053+kevinle623@users.noreply.github.com>
2024-03-03 18:28:03 -0500

    TCES-51 tim timeline backend api (#56)
    * set up apis for timelines
    
    * added documentation
    
    * set automatic entries
    
    * fixed tests
    
    * fixed job types error
    
    * Update job_lead_timeline_entries.js
    
    * fixed client table pointing from client timeline model
    
    * fixed endpoints
    
    * fixed lint
    
    * made clearer documentation
    
    * current adjustments to be continued...
    
    * fixed tests
    
    * fixed lint
    
    * some clean up
    
    * some tweaks as per comments
    
    * use req.user instead of getting user from body
    
    * added places to use user instead
    
    * some fixes
    
    * Update addEmployerTimelineEntry.js
    
    * changed to user.id
    
    * fixed id bug
    
    * fixed tests
    
    * Small fixes
    
    ---------
    
    Co-authored-by: Daniel Dervishi <d.dervishi@mail.utoronto.ca>

---
+++ server/src/controllers/client_timeline_entries/addClientTimelineEntry.js
@@ -0,0 +1,78 @@
+const logger = require("pino")();
+const ClientTimelineEntry = require("../../models/client_timeline_entry.model");
+const JobLead = require("../../models/job_lead.model");
+const Employer = require("../../models/employer.model");
+
+const addClientTimelineEntryRequestHandler = async (req, res) => {
+  try {
+    // eslint-disable-next-line camelcase
+    const { type, title, body, client, job_lead } = req.body.entry;
+
+    const { user } = req;
+
+    const validTypes = ["contact", "note"];
+    if (!validTypes.includes(type)) {
+      return res.status(400).json({
+        status: "fail",
+        message: "Type not defined or is not valid",
+        data: null,
+      });
+    }
+
+    if (
+      ["update", "contact"].includes(type) &&
+      (!user || !title || !body || !client)
+    ) {
+      return res.status(400).json({
+        status: "fail",
+        message: `For type ${type}, user, title, body, and client must be defined`,
+        data: null,
+      });
+    }
+
+    if (type === "note" && (!user || !title || !body)) {
+      return res.status(400).json({
+        status: "fail",
+        message: "For type note, user, title, and body must be defined",
+        data: null,
+      });
+    }
+
+    const jobLead = await JobLead.findOne({ where: { id: job_lead } });
+    const employer = await Employer.findOne({
+      where: { id: jobLead.employer },
+    });
+
+    const clientTimelineEntry = await ClientTimelineEntry.create({
+      date_added: new Date(),
+      type,
+      title,
+      body,
+      client,
+      employer: employer.id,
+      // eslint-disable-next-line camelcase
+      job_lead,
+      user: user.id,
+    });
+
+    return res.status(200).json({
+      status: "success",
+      message: "created client timeline entry",
+      data: { clientTimelineEntry },
+    });
+  } catch (err) {
+    if (err.name === "SequelizeUniqueConstraintError") {
+      // This means that either user or owner is not a valid user
+      return res.status(400).json({
+        status: "fail",
+        message: "Either owner or creator is not a valid user",
+        data: null,
+      });
+    }
+
+    logger.error(`Unexpected error thrown: ${err}`);
+    res.status(500).json({ status: "error", message: "Internal server error" });
+  }
+};
+
+module.exports = addClientTimelineEntryRequestHandler;
