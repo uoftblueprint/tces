6e738ceb2f469950180f01a1dd7efc100c745787
Kevin Le <74465053+kevinle623@users.noreply.github.com>
2024-05-01 15:04:55 -0400

    fixed component usage

--- client/src/components/shared/error-screen-component/index.jsx
+++ client/src/components/shared/error-screen-component/index.jsx
@@ -1,8 +1,8 @@
-import Error500Component from "../error-components/500-error-component";
+import Error500 from "../../../pages/errors/500-error";
 
 function ErrorComponent() {
   return (
-      <Error500Component/>
+      <Error500/>
   );
 }
 
