b8aa7dbeeb6c45f1475196eeb53b14e677203e33
Harshith Latchupatula <harshithl1777@gmail.com>
2024-11-07 18:22:10 -0500

    [TCES-19] Create JobPosting Model (#109)
    * feat: create JobPosting model
    
    * chore: run formatter
    
    * update soe fields to allow NULLs
    
    Signed-off-by: root <root@LAPTOP-9VOLP0N6>
    
    * fix prettier issues
    
    * update rate_of_pay formatting
    
    ---------
    
    Signed-off-by: root <root@LAPTOP-9VOLP0N6>
    Co-authored-by: root <root@LAPTOP-9VOLP0N6>
    Co-authored-by: Kashish Mittal <kmittal@redhat.com>

---
+++ server/src/models/job_posts.model.js
@@ -0,0 +1,100 @@
+require("dotenv").config();
+const { DataTypes } = require("sequelize");
+const { sequelize } = require("../configs/sequelize");
+const User = require("./user.model");
+
+const JobPosting = sequelize.define(
+  "job_postings",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    employer: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    location: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    hours_per_week: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      validate: {
+        min: 1,
+      },
+    },
+    rate_of_pay_min: {
+      type: DataTypes.DECIMAL,
+      allowNull: true,
+    },
+    rate_of_pay_max: {
+      type: DataTypes.DECIMAL,
+      allowNull: true,
+    },
+    rate_of_pay_frequency: {
+      type: DataTypes.ENUM(
+        "Hourly",
+        "Weekly",
+        "Annually",
+        "Commission",
+        "Base & Commission",
+      ),
+      allowNull: true,
+    },
+    job_type: {
+      type: DataTypes.ARRAY(
+        DataTypes.ENUM("Part-time", "Full-time", "Contract", "Permanent"),
+      ),
+      allowNull: true,
+    },
+    close_date: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+    job_description: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    custom_questions: {
+      type: DataTypes.ARRAY(DataTypes.STRING),
+      allowNull: true,
+    },
+    creator: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: User,
+        key: "id",
+      },
+    },
+    state: {
+      type: DataTypes.ENUM("Draft", "Active", "Inactive"),
+      allowNull: false,
+      defaultValue: "Draft",
+    },
+  },
+  {
+    sequelize,
+    timestamps: true,
+    hooks: {
+      beforeUpdate: (job) => {
+        if (
+          job.state === "Active" &&
+          job.close_date &&
+          job.close_date < new Date()
+        ) {
+          job.setDataValue("state", "Inactive");
+        }
+      },
+    },
+  },
+);
+
+module.exports = JobPosting;
