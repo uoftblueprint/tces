74e8db6ed1c56798ecd1072ddfe9e267df672e0e
Michelle <michh2005@gmail.com>
2024-11-08 01:49:53 -0500

    added new compoensation rate type constant

--- client/src/utils/contants.js
+++ client/src/utils/contants.js
@@ -2,3 +2,4 @@
 
 export const JOB_TYPES = ["Full Time", "Part Time", "Casual", "On-Call"];
 export const STATUS_TYPES = ["Active", "R&I", "Closed"];
+export const COMPENSATION_RATES = ['Hourly', 'Weekly', 'Annually', 'Commision', 'Base & Commission']
