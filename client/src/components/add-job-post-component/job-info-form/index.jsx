f95dfdab994c5593518d993ab45888f866eee2eb
Michelle <michh2005@gmail.com>
2024-11-12 05:06:00 -0500

    removed unnecessary props

--- client/src/components/add-job-post-component/job-info-form/index.jsx
+++ client/src/components/add-job-post-component/job-info-form/index.jsx
@@ -14,20 +14,16 @@ import { LocalizationProvider, DatePicker } from "@mui/x-date-pickers";
 import dayjs from "dayjs";
 import { JobLeadContainer, H3 } from "../index.styles";
 import { JOB_TYPES, COMPENSATION_RATES } from "../../../utils/contants";
-// import ErrorScreenComponent from "../shared/error-screen-component";
 
-function AddJobDetails({ jobPostData, setJobPostData, isAddEmployer }) {
+function AddJobDetails({ jobPostData, setJobPostData }) {
   const handleInputChange = (input, field) => {
     const updatedApplicationFields = { ...jobPostData, [field]: input };
     setJobPostData(updatedApplicationFields);
   };
   return (
     // TODOs
-    // 1. get rid of required star beside label names
     // 2. ask about employment type label, really hard to make it the same native transition :skull
-    // 3. add required bottom label to additional information and make it aligned w/the textarea
-    // 4. change all labels, ids and setJobPostData paramters to match new input types
-    <JobLeadContainer key={jobPostData.id}>
+    <JobLeadContainer>
       <div
         style={{
           display: "flex",
@@ -42,45 +38,46 @@ function AddJobDetails({ jobPostData, setJobPostData, isAddEmployer }) {
       <TextField
         fullWidth
         sx={{ m: 1, width: "96%" }}
-        id={`title-${jobPostData.id}`}
+        id="title"
         label="Job Title"
         value={jobPostData.title}
         onChange={(e) => handleInputChange(e.target.value, "title")}
-        helperText={isAddEmployer ? "" : "*Required"}
-        required={!isAddEmployer}
+        helperText="*Required"
+        required
+        InputLabelProps={{ required: false }}
       />
 
       {/* Employer Name Field */}
       <TextField
         fullWidth
         sx={{ m: 1, width: "96%" }}
-        id={`employer-${jobPostData.id}`}
+        id="employer"
         label="Employer Name"
         value={jobPostData.employer}
         onChange={(e) => handleInputChange(e.target.value, "employer")}
-        helperText={isAddEmployer ? "" : "*Required"}
-        required={!isAddEmployer}
+        helperText="*Required"
+        required
+        InputLabelProps={{ required: false }}
       />
 
       {/* Location Field */}
       <TextField
         fullWidth
         sx={{ m: 1, width: "96%" }}
-        id={`location-${jobPostData.id}`}
+        id="location"
         label="Location"
         value={jobPostData.location}
         onChange={(e) => handleInputChange(e.target.value, "location")}
-        helperText={isAddEmployer ? "" : "*Required"}
-        required={!isAddEmployer}
+        helperText="*Required"
+        required
+        InputLabelProps={{ required: false }}
       />
 
       {/* Minimum Compensation Field */}
       <FormControl fullWidth sx={{ m: 1, width: "47%" }}>
-        <InputLabel id={`minCompensationLabel-${jobPostData.id}`}>
-          Minimum Compensation
-        </InputLabel>
+        <InputLabel id="minCompensationLabel">Minimum Compensation</InputLabel>
         <OutlinedInput
-          id={`minCompensation-${jobPostData.id}`}
+          id="minCompensationLabel"
           type="number"
           startAdornment={<InputAdornment position="start">$</InputAdornment>}
           label="Minimum Compensation"
@@ -91,17 +88,16 @@ function AddJobDetails({ jobPostData, setJobPostData, isAddEmployer }) {
             if (/^\d*\.?\d*$/.test(value))
               handleInputChange(value, "minCompensation");
           }}
-          required={!isAddEmployer}
+          required
+          InputLabelProps={{ required: false }}
         />
       </FormControl>
 
       {/* Maximum Compensation Field */}
       <FormControl fullWidth sx={{ m: 1, width: "47%" }}>
-        <InputLabel id={`maxCompensationLabel-${jobPostData.id}`}>
-          Maximum Compensation
-        </InputLabel>
+        <InputLabel id="maxCompensationLabel">Maximum Compensation</InputLabel>
         <OutlinedInput
-          id={`maxCompensation-${jobPostData.id}`}
+          id="maxCompensationLabel"
           type="number"
           inputProps={{ min: 0 }}
           startAdornment={<InputAdornment position="start">$</InputAdornment>}
@@ -112,23 +108,23 @@ function AddJobDetails({ jobPostData, setJobPostData, isAddEmployer }) {
             if (/^\d*\.?\d*$/.test(value))
               handleInputChange(value, "maxCompensation");
           }}
-          required={!isAddEmployer}
+          required
+          InputLabelProps={{ required: false }}
         />
       </FormControl>
 
       {/* Compensation Rate Field */}
       <FormControl fullWidth sx={{ m: 1, width: "47%" }}>
-        <InputLabel id={`employmentTypeLabel-${jobPostData.id}`}>
-          Compensation rate
-        </InputLabel>
+        <InputLabel id="employmentTypeLabel">Compensation rate</InputLabel>
         <Select
           sx={{ textAlign: "left" }}
-          labelId={`employmentTypeLabel-${jobPostData.id}`}
-          id={`employmentType-${jobPostData.id}`}
+          labelId="employmentTypeLabel"
+          id="employmentTypeLabel"
           value={jobPostData.employmentType}
           label="Compensation rate"
           onChange={(e) => handleInputChange(e.target.value, "employmentType")}
-          required={!isAddEmployer}
+          required
+          InputLabelProps={{ required: false }}
         >
           {COMPENSATION_RATES.map((jobType) => (
             <MenuItem key={jobType} value={jobType}>
@@ -136,14 +132,14 @@ function AddJobDetails({ jobPostData, setJobPostData, isAddEmployer }) {
             </MenuItem>
           ))}
         </Select>
-        {!isAddEmployer && <FormHelperText>*Required</FormHelperText>}
+        <FormHelperText>*Required</FormHelperText>
       </FormControl>
 
       {/* Hours Per Week Field */}
       <TextField
         fullWidth
         sx={{ m: 1, width: "47%" }}
-        id={`hoursPerWeek-${jobPostData.id}`}
+        id="hoursPerWeek"
         type="number"
         label="Hours per week"
         inputProps={{ min: 0 }}
@@ -153,7 +149,8 @@ function AddJobDetails({ jobPostData, setJobPostData, isAddEmployer }) {
           if (/^\d*\.?\d*$/.test(value))
             handleInputChange(value, "hoursPerWeek");
         }}
-        required={!isAddEmployer}
+        required
+        InputLabelProps={{ required: false }}
       />
 
       {/* Employment Type Field */}
@@ -161,12 +158,13 @@ function AddJobDetails({ jobPostData, setJobPostData, isAddEmployer }) {
         <InputLabel>Employment Type</InputLabel>
         <Select
           sx={{ textAlign: "left" }}
-          labelId={`employmentTypeLabel-${jobPostData.id}`}
-          id={`employmentType-${jobPostData.id}`}
+          labelId="employmentTypeLabel"
+          id="employmentTypeLabel"
           value={jobPostData.employmentType}
           label="Employment Type"
           onChange={(e) => handleInputChange(e.target.value, "employmentType")}
-          required={!isAddEmployer}
+          required
+          InputLabelProps={{ required: false }}
         >
           {JOB_TYPES.map((jobType) => (
             <MenuItem key={jobType} value={jobType}>
@@ -174,46 +172,48 @@ function AddJobDetails({ jobPostData, setJobPostData, isAddEmployer }) {
             </MenuItem>
           ))}
         </Select>
-        {!isAddEmployer && <FormHelperText>*Required</FormHelperText>}
+        <FormHelperText>*Required</FormHelperText>
       </FormControl>
 
       {/* Creation Date Picker */}
       <LocalizationProvider dateAdapter={AdapterDayjs}>
         <DatePicker
-          id={`creationDate-${jobPostData.id}`}
+          id="expirationDate"
           label="Creation Date"
           fullWidth
           sx={{ m: 1, width: "47%" }}
           value={jobPostData.creationDate}
           onChange={(newValue) =>
-            handleInputChange(newValue, jobPostData.id, "creationDate")
+            handleInputChange(newValue, jobPostData, "creationDate")
           }
           minDate={dayjs()}
           renderInput={(params) => (
             // eslint-disable-next-line
             <TextField {...params} error={false} helperText="" required />
           )}
-          required={!isAddEmployer}
+          required
+          InputLabelProps={{ required: false }}
         />
       </LocalizationProvider>
 
       {/* Expiration Date Picker */}
       <LocalizationProvider dateAdapter={AdapterDayjs}>
         <DatePicker
-          id={`expirationDate-${jobPostData.id}`}
+          id="expirationDate"
           label="Expiration Date"
           fullWidth
           sx={{ m: 1, width: "47%" }}
           value={jobPostData.expirationDate}
           minDate={dayjs()}
           onChange={(newValue) =>
-            handleInputChange(newValue, jobPostData.id, "expirationDate")
+            handleInputChange(newValue, jobPostData, "expirationDate")
           }
           renderInput={(params) => (
             // eslint-disable-next-line
             <TextField {...params} error={false} helperText="" required />
           )}
-          required={!isAddEmployer}
+          required
+          InputLabelProps={{ required: false }}
         />
       </LocalizationProvider>
 
@@ -222,14 +222,15 @@ function AddJobDetails({ jobPostData, setJobPostData, isAddEmployer }) {
       <TextField
         fullWidth
         sx={{ m: 1, width: "96%" }}
-        id={`description-${jobPostData.id}`}
+        id="description"
         label="Additional Information"
         multiline
         rows={4}
         value={jobPostData.additionalInfo}
-        InputLabelProps={{ shrink: true }}
+        InputLabelProps={{ shrink: true, required: false }}
+        helperText="*Required"
         onChange={(e) => handleInputChange(e.target.value, "additionalInfo")}
-        required={!isAddEmployer}
+        required
       />
     </JobLeadContainer>
   );
@@ -239,11 +240,6 @@ AddJobDetails.propTypes = {
   // eslint-disable-next-line react/forbid-prop-types
   jobPostData: PropTypes.array.isRequired,
   setJobPostData: PropTypes.func.isRequired,
-  isAddEmployer: PropTypes.bool,
-};
-
-AddJobDetails.defaultProps = {
-  isAddEmployer: false, // or whatever your default value should be
 };
 
 export default AddJobDetails;
