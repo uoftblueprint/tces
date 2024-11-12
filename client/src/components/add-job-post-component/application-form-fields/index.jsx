a597cc523899ec16521385caca69c720e8c3aee5
Michelle <michh2005@gmail.com>
2024-11-12 04:54:37 -0500

    removed unnecessary props

--- client/src/components/add-job-post-component/application-form-fields/index.jsx
+++ client/src/components/add-job-post-component/application-form-fields/index.jsx
@@ -1,4 +1,3 @@
-import PropTypes from "prop-types";
 import UploadFileIcon from "@mui/icons-material/UploadFile";
 import {
   Box,
@@ -7,24 +6,17 @@ import {
   IconButton,
   InputLabel,
   Select,
-  MenuItem,
   TextField,
   FormHelperText,
   Typography,
 } from "@mui/material";
 import { ButtonL, JobLeadContainer, H3 } from "../index.styles";
-import { COMPENSATION_RATES } from "../../../utils/contants";
 import styles from "./index.module.css";
 
-function AddApplicationFields({ jobPostData, setJobPostData, isAddEmployer }) {
+function AddApplicationFields() {
   return (
-    // TODOs
-    // 1. get rid of required star beside label names
-    // 2. ask about employment type label, really hard to make it the same native transition :skull
-    // 3. add required bottom label to additional information and make it aligned w/the textarea
-    // 4. change all labels, ids and setJobPostData paramters to match new input types
     <>
-      <JobLeadContainer key={jobPostData.id}>
+      <JobLeadContainer>
         <div
           style={{
             display: "flex",
@@ -42,12 +34,9 @@ function AddApplicationFields({ jobPostData, setJobPostData, isAddEmployer }) {
           className={styles.dotted}
           sx={{ m: 1, width: "47%" }}
           label="Name"
-          value={jobPostData.title}
-          helperText={isAddEmployer ? "" : "*Required"}
-          required={!isAddEmployer}
-          InputLabelProps={{
-            required: false,
-          }}
+          helperText="*Required"
+          required
+          InputLabelProps={{ required: false }}
         />
 
         {/* Email Field */}
@@ -57,9 +46,9 @@ function AddApplicationFields({ jobPostData, setJobPostData, isAddEmployer }) {
           className={styles.dotted}
           sx={{ m: 1, width: "47%" }}
           label="Email Address"
-          value={jobPostData.employer}
-          helperText={isAddEmployer ? "" : "*Required"}
-          required={!isAddEmployer}
+          helperText="*Required"
+          required
+          InputLabelProps={{ required: false }}
         />
 
         {/* Phone Field */}
@@ -69,12 +58,9 @@ function AddApplicationFields({ jobPostData, setJobPostData, isAddEmployer }) {
           className={styles.dotted}
           sx={{ m: 1, width: "47%" }}
           label="Phone"
-          value={jobPostData.location}
-          helperText={isAddEmployer ? "" : "*Required"}
-          required={!isAddEmployer}
-          InputLabelProps={{
-            required: false,
-          }}
+          helperText="*Required"
+          required
+          InputLabelProps={{ required: false }}
         />
 
         {/* Postal Code Field */}
@@ -84,38 +70,19 @@ function AddApplicationFields({ jobPostData, setJobPostData, isAddEmployer }) {
           className={styles.dotted}
           sx={{ m: 1, width: "47%" }}
           label="Postal Code"
-          value={jobPostData.location}
-          helperText={isAddEmployer ? "" : "*Required"}
-          required={!isAddEmployer}
-          InputLabelProps={{
-            required: false,
-          }}
+          helperText="*Required"
+          required
+          InputLabelProps={{ required: false }}
         />
 
         {/* Status In Canada Field */}
         <FormControl disabled fullWidth sx={{ m: 1, width: "96%" }}>
-          <InputLabel id={`employmentTypeLabel-${jobPostData.id}`}>
-            Status In Canada
-          </InputLabel>
-          <Select
-            className={styles.dotted}
-            sx={{ textAlign: "left" }}
-            labelId={`employmentTypeLabel-${jobPostData.id}`}
-            value={jobPostData.employmentType}
-            label="Compensation rate"
-            required={!isAddEmployer}
-          >
-            {COMPENSATION_RATES.map((jobType) => (
-              <MenuItem key={jobType} value={jobType}>
-                {jobType}
-              </MenuItem>
-            ))}
-          </Select>
-          {!isAddEmployer && <FormHelperText>*Required</FormHelperText>}
+          <InputLabel>Status In Canada</InputLabel>
+          <Select className={styles.dotted} required />
+          <FormHelperText>*Required</FormHelperText>
         </FormControl>
 
         {/* Cover Letter Field */}
-        {/* TODO ADD REQUIRED LABEL */}
         <TextField
           disabled
           fullWidth
@@ -124,17 +91,14 @@ function AddApplicationFields({ jobPostData, setJobPostData, isAddEmployer }) {
           label="Cover Letter"
           multiline
           rows={4}
-          value={jobPostData.description}
           InputLabelProps={{ shrink: true, required: false }}
-          onChange={(e) =>
-            setJobPostData(e.target.value, jobPostData.id, "description")
-          }
-          required={!isAddEmployer}
+          required
         />
 
-        {/* Add this to next job posting page */}
-        <Box sx={{ boxSizing: "border-box", margin: "8px" }}>
-          <Typography color="#9E9E9E" pb="13px">*Upload Resume</Typography>
+        <Box sx={{ margin: "8px" }}>
+          <Typography color="#9E9E9E" pb="13px">
+            *Upload Resume
+          </Typography>
           <Container
             sx={{
               border: "1px dashed #E0E0E0",
@@ -148,18 +112,17 @@ function AddApplicationFields({ jobPostData, setJobPostData, isAddEmployer }) {
             }}
           >
             <Container sx={{ width: "auto" }}>
-              <IconButton
-                aria-label="Example"
-                sx={{ backgroundColor: "#E6ECFB" }}
-              >
+              <IconButton sx={{ backgroundColor: "#E6ECFB" }}>
                 <UploadFileIcon />
               </IconButton>
             </Container>
             <Typography variant="body1" pb="10px" pt="10px">
-              <span className={styles.underline}>Click to upload</span> or drag and drop
+              <span className={styles.underline}>Click to upload</span>
+              or drag and drop
             </Typography>
-            <Typography variant="body2">SVG, PNG, JPG or GIF (max. 3MB)</Typography>
-
+            <Typography variant="body2">
+              SVG, PNG, JPG or GIF (max. 3MB)
+            </Typography>
           </Container>
         </Box>
       </JobLeadContainer>
@@ -175,16 +138,5 @@ function AddApplicationFields({ jobPostData, setJobPostData, isAddEmployer }) {
     </>
   );
 }
-
-AddApplicationFields.propTypes = {
-  // eslint-disable-next-line react/forbid-prop-types
-  jobPostData: PropTypes.array.isRequired,
-  setJobPostData: PropTypes.func.isRequired,
-  isAddEmployer: PropTypes.bool,
-};
-
-AddApplicationFields.defaultProps = {
-  isAddEmployer: false, // or whatever your default value should be
-};
 
 export default AddApplicationFields;
