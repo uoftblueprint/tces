a3be593564a132c76469d24e9a03785c675647fe
William Nhut Lam <williamntlam@gmail.com>
2024-11-15 16:28:34 -0500

    chore: provided logic of code implementation for Job Posts POST requests before implementation.

--- server/src/controllers/job_posts/addJobPost.js
+++ server/src/controllers/job_posts/addJobPost.js
@@ -34,4 +34,44 @@ const logger = require("pino")();
  * - timestamps: Automatically manages createdAt and updatedAt fields
  */
 
-const addJobPostRequestHandler = async (req, res) => {};
+const addJobPostRequestHandler = async (req, res) => {
+  // ! I think there are two different types of Job posts.
+  // ! From this, there are different required attributes.
+  // ! The two types is a Complete Job Post and a Draft Job Post
+  // ! How will the two Draft types be distinguished from the client side?
+  // ! Probably by a value in the request body.
+  // * state: "Draft", "Active", "Inactive"
+  // ! So the state is the attribute used to determine what type of Job Post object it is.
+  // ! The default value is a draft if no other value is inputted for state.
+  // ? Is there anything that needs to be done with the state is 'Inactive'?
+  // ? I think I need to check if the current date is after the closing date for the state 'Inactive'
+  // ? If this is the case, I set the state of the current Job Application to be Inactive.
+  // ? Hypothetically, this 'Inactive' state should be done automatically in the Model file I'm assuming.
+  // ? Okay, I checked and looks like it is already done.
+  // ? It will automatically be updated when a single CRUD request tries to mutate the respective object.
+  // TODO: Check in the request body what the state of the Job Post is.
+  // TODO: If the Job Post is a Draft => Only the title is required. The creator field should also be filled
+  // TODO with the current user ID. Apparently, since passport.js is used this can be done directly with
+  // TODO req.user.id!
+  // TODO: Otherwise, check if the state is active => In this case, I need to make sure that all required attributes
+  // TODO are given.
+  // TODO: These are the required attributes if the Job Posting is not a draft.
+  /*
+   * title
+   * employer
+   * location
+   * hours_per_week
+   * rate_of_pay_min
+   * rate_of_pay_max
+   * rate_of_pay_frequency
+   * job_type
+   * close_date
+   * job_description
+   */
+  // TODO: Since the model file for Job Posts automatically checks if the current date is beyond the closing date
+  // TODO I don't think I need to do anything then. I think it's mainly for GET requests where the state
+  // TODO will be updated when the client gets the Job Posts.
+  // TODO: Seems like GET requests won't update the 'state' attribute automatically, but there can be
+  // TODO automatic background tasks (like a cron job) that can periodically check
+  // TODO: I should ask about this to double check.
+};
