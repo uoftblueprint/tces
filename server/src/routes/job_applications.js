f1c8df9f3b6c4327c4043b70f218802edbdb68ec
William Nhut Lam <williamntlam@gmail.com>
2024-11-17 14:31:40 -0500

    feat: added router code for Job Application POST request

--- server/src/routes/job_applications.js
+++ server/src/routes/job_applications.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const multer = require("multer");
+const path = require("path");
 const getAllJobApplicationsRequestHandler = require("../controllers/job_applications/getAllJobApplications");
 const getOneJobApplicationRequestHandler = require("../controllers/job_applications/getOneJobApplication");
+const addJobApplicationRequestHandler = require("../controllers/job_applications/addJobApplication");
+
+const upload = multer({ dest: path.join(__dirname, "..", "uploads") });
 
 const router = express.Router();
 
@@ -12,6 +17,10 @@ router.get("/", async (req, res) => {
 // Route to get job applications by name
 router.get("/:name", async (req, res) => {
   return getOneJobApplicationRequestHandler(req, res);
+});
+
+router.post("/", upload.single("resume"), async (req, res) => {
+  return addJobApplicationRequestHandler(req, res);
 });
 
 module.exports = router;
