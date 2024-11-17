a302de96062f736bf0c0e013ffd8484f2d54a2c8
Michelle Huang <50971945+1michhu1@users.noreply.github.com>
2024-11-17 12:54:35 -0500

    Update client/src/utils/contants.js
    Co-authored-by: Kashish Mittal <113269381+04kash@users.noreply.github.com>

--- client/src/utils/contants.js
+++ client/src/utils/contants.js
@@ -2,4 +2,4 @@
 
 export const JOB_TYPES = ["Full Time", "Part Time", "Casual", "On-Call"];
 export const STATUS_TYPES = ["Active", "Draft", "Inactive"];
-export const COMPENSATION_RATES = ['Hourly', 'Weekly', 'Annually', 'Commision', 'Base & Commission']
+export const COMPENSATION_RATES = ['Hourly', 'Weekly', 'Annually', 'Commission', 'Base and Commission']
