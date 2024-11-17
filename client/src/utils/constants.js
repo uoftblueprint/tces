fdaab07c5e805890a1b4abaeca7cd435fd7b244b
Michelle <michh2005@gmail.com>
2024-11-17 12:57:12 -0500

    fixed linter isue, updated job_types list

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,5 +1,19 @@
 // all constants stored here
 
-export const JOB_TYPES = ["Full Time", "Part Time", "Casual", "On-Call"];
+export const JOB_TYPES = [
+  "Contract",
+  "Freelance",
+  "Full-time",
+  "Internship",
+  "Part-time",
+  "Permanent",
+  "Seasonal",
+];
 export const STATUS_TYPES = ["Active", "Draft", "Inactive"];
-export const COMPENSATION_RATES = ['Hourly', 'Weekly', 'Annually', 'Commision', 'Base & Commission']
+export const COMPENSATION_RATES = [
+  "Hourly",
+  "Weekly",
+  "Annually",
+  "Commision",
+  "Base & Commission",
+];
