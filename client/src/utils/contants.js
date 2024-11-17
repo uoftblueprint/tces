3e14d85b6d7836a17509d1006b666d232bbc2680
Michelle Huang <50971945+1michhu1@users.noreply.github.com>
2024-11-17 12:49:31 -0500

    Update client/src/utils/contants.js
    Co-authored-by: Kashish Mittal <113269381+04kash@users.noreply.github.com>

--- client/src/utils/contants.js
+++ client/src/utils/contants.js
@@ -1,5 +1,5 @@
 // all constants stored here
 
 export const JOB_TYPES = ["Full Time", "Part Time", "Casual", "On-Call"];
-export const STATUS_TYPES = ["Active", "R&I", "Closed"];
+export const STATUS_TYPES = ["Active", "Draft", "Inactive"];
 export const COMPENSATION_RATES = ['Hourly', 'Weekly', 'Annually', 'Commision', 'Base & Commission']
