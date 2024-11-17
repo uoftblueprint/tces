51ab43ec070b93286cdfbcd700fd1c0bebeb1f3c
William Nhut Lam <williamntlam@gmail.com>
2024-11-17 14:21:32 -0500

    chore: added uploadJobApplication to job_applications_api/js in client/src/utils

--- client/src/utils/job_applications_api.js
+++ client/src/utils/job_applications_api.js
@@ -63,4 +63,23 @@ const fetchJobApplicationsByApplicantName = async ({
   }
 };
 
-export { fetchAllJobApplications, fetchJobApplicationsByApplicantName };
+const uploadJobApplication = async (file) => {
+  const formData = new FormData();
+  formData.append("resume", file); // Assuming "resume" is the field name for the file
+
+  // eslint-disable-next-line no-useless-catch
+  const response = await fetch(`${REACT_APP_API_BASE_URL}/job_applications`, {
+    method: "POST",
+    credentials: "include",
+    body: formData,
+  });
+  return response;
+};
+
+export default uploadJobApplication;
+
+export {
+  fetchAllJobApplications,
+  fetchJobApplicationsByApplicantName,
+  uploadJobApplication,
+};
