d0cffc46b203a149522eb36aa82fcdb91e0cbab0
William Nhut Lam <williamntlam@gmail.com>
2024-11-17 14:03:49 -0500

    feat: added frontend API calls to job application get requests

---
+++ client/src/utils/job_applications_api.js
@@ -0,0 +1,66 @@
+const { REACT_APP_API_BASE_URL } = process.env;
+
+const fetchAllJobApplications = async ({
+  page = 1,
+  pageSize = 10,
+  jobPostingId = null,
+} = {}) => {
+  const url = new URL(`${REACT_APP_API_BASE_URL}/job_applications`);
+
+  // Set query parameters for pagination and filtering by job posting ID
+  if (page) url.searchParams.append("page", page);
+  if (pageSize) url.searchParams.append("pageSize", pageSize);
+  if (jobPostingId) url.searchParams.append("job_posting_id", jobPostingId);
+
+  try {
+    const response = await fetch(url, {
+      method: "GET",
+      credentials: "include", // Include credentials for authentication if necessary
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) throw new Error("Failed to fetch job applications");
+
+    return await response.json();
+  } catch (error) {
+    console.error("Error fetching all job applications:", error);
+    throw error;
+  }
+};
+
+const fetchJobApplicationsByApplicantName = async ({
+  name,
+  page = 1,
+  pageSize = 10,
+} = {}) => {
+  if (!name)
+    throw new Error("Name is required to fetch job applications by applicant");
+
+  const url = new URL(`${REACT_APP_API_BASE_URL}/job_applications/${name}`);
+
+  // Set query parameters for pagination
+  if (page) url.searchParams.append("page", page);
+  if (pageSize) url.searchParams.append("pageSize", pageSize);
+
+  try {
+    const response = await fetch(url, {
+      method: "GET",
+      credentials: "include", // Include credentials if authentication is needed
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok)
+      throw new Error("Failed to fetch job applications by applicant");
+
+    return await response.json();
+  } catch (error) {
+    console.error("Error fetching job applications by applicant:", error);
+    throw error;
+  }
+};
+
+export { fetchAllJobApplications, fetchJobApplicationsByApplicantName };
