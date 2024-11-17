7845e3f4360a1a55d11c67048de80dd0ccea81e6
William Nhut Lam <163342709+williamntlam@users.noreply.github.com>
2024-11-17 14:16:30 -0500

    [TCES-27]: GET Request for Job Applications (#119)
    * feat: rough implementation of getAllJobApplications GET request. Returns mainly three things: the total number of job applications, all job applications saved in the database, and the unique names of all job applicants.
    
    * refactor: added try catch statement for getAllJobApplications GET request
    
    * feat: added functionality to get all job applications for a job post in getAllJobApplications.js if a job posting id is given.
    
    * feat: implemented getOneJobApplication.js to filter through job applications through an applicant's name
    
    * feat: added routing logic and connected it to index.js
    
    * fix: fixed compatibility errors with Job Posting
    
    * chore: ran linter and formatter
    
    * chore: run linter and formatter
    
    * feat: updated job_posts_model.js file
    
    * chore: removed unnecessary comments
    
    * feat: added api calls to api.js in the frontend
    
    * fix: applications are returned in most recent to least recent order
    
    * fix: trying to include createdAt field into the response body
    
    * feat: added frontend API calls to job application get requests
    
    * chore: deleted get application functions from api.js

---
+++ server/src/routes/job_applications.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const getAllJobApplicationsRequestHandler = require("../controllers/job_applications/getAllJobApplications");
+const getOneJobApplicationRequestHandler = require("../controllers/job_applications/getOneJobApplication");
+
+const router = express.Router();
+
+// Route to get all job applications, with optional job_posting_id filter
+router.get("/", async (req, res) => {
+  return getAllJobApplicationsRequestHandler(req, res);
+});
+
+// Route to get job applications by name
+router.get("/:name", async (req, res) => {
+  return getOneJobApplicationRequestHandler(req, res);
+});
+
+module.exports = router;
