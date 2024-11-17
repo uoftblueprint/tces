62a5a6ca90fade1de98694c3268c2dd3e1ad46e6
lana <lanawehbeh12@gmail.com>
2024-11-17 13:27:12 -0500

    Added call to endpoint

--- client/src/utils/job_posts_api.js
+++ client/src/utils/job_posts_api.js
@@ -19,6 +19,39 @@ const createJobPost = async (jobPostData) => {
   return result;
 };
 
+const modifyJobLead = async (modifiedJobPost) => {
+  const modifyJobPostBody = {
+    values: {
+      title: modifiedJobPost.owner,
+      employer: modifiedJobPost.jobTitle,
+      location: modifiedJobPost.location,
+      hours_per_week: modifiedJobPost.hours_per_week,
+      rate_of_pay_min: modifiedJobPost.rate_of_pay_min,
+      rate_of_pay_max: modifiedJobPost.rate_of_pay_max,
+      rate_of_pay_frequency: modifiedJobPost.rate_of_pay_frequency,
+      job_type: modifiedJobPost.job_type,
+      close_date: modifiedJobPost.close_date,
+      job_description: modifiedJobPost.job_description,
+      custom_questions: modifiedJobPost.custom_questions,
+      state: modifiedJobPost.state,
+    },
+  };
+
+  // eslint-disable-next-line no-useless-catch
+  const response = await fetch(
+    `${REACT_APP_API_BASE_URL}/job_posts/${modifiedJobPost.job_post_id}`,
+    {
+      method: "PUT",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(modifyJobPostBody),
+    },
+  );
+  return response;
+};
+
 async function deleteJobPosting(jobPostingId) {
   try {
     const response = await fetch(
@@ -47,4 +80,4 @@ async function deleteJobPosting(jobPostingId) {
   }
 }
 
-module.exports = { createJobPost, deleteJobPosting };
+module.exports = { createJobPost, modifyJobLead ,deleteJobPosting };
