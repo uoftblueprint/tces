c49cc8215b37f709caa1fa6d9e2db91bd4daeab8
Michelle <michh2005@gmail.com>
2024-11-18 01:41:31 -0500

    deleted contants (replaced with constants.js)

--- client/src/utils/contants.js
+++
@@ -1,6 +0,0 @@
-// all constants stored here
-
-export const JOB_TYPES = ["Full Time", "Part Time", "Casual", "On-Call"];
-export const STATUS_TYPES = ["Active", "Draft", "Inactive"];
-export const COMPENSATION_RATES = ['Hourly', 'Weekly', 'Annually', 'Commission', 'Base and Commission']
-
