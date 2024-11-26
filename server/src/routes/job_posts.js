6c503a218c185b1d9c0bb89364eef421b5796f13
William Nhut Lam <williamntlam@gmail.com>
2024-11-25 21:29:25 -0500

    refactor: changed job_posts.js in routes folder to match employer_timeline_entries.js format

--- server/src/routes/job_posts.js
+++ server/src/routes/job_posts.js
@@ -1,9 +1,38 @@
 const express = require("express");
-const deleteJobPostHandler = require("../controllers/job_posts/deleteJobPost"); // Update the path to match your project structure
 
 const router = express.Router();
 
-// Route for deleting a Job Post and its associated Job Applications
-router.delete("/:job_posting_id", deleteJobPostHandler);
+const isLoggedIn = require("../middlewares/auth/isLoggedIn");
+
+const deleteJobPostHandler = require("../controllers/job_posts/deleteJobPost");
+
+/**
+ * Delete a Job Post and its Associated Job Applications
+ *
+ * Deletes a job post identified by its `job_posting_id` along with all the associated job applications.
+ *
+ * @type DELETE
+ *
+ * @param {string} job_posting_id - The ID of the job posting to delete. This should be passed as a route parameter.
+ *
+ * Example Request:
+ * DELETE /:job_posting_id
+ *
+ * Example Successful Response:
+ * {
+ *   "status": success,
+ *   "message": "Job Posts and all associated Job Applications have been successfully deleted.",
+ *   "data": null
+ * }
+ *
+ * Example Error Response:
+ * {
+ *   "error": error.message
+ * }
+ *
+ * Middleware:
+ * - isLoggedIn: Ensures that the user is authenticated before allowing the deletion operation.
+ */
+router.delete("/:job_posting_id", isLoggedIn, deleteJobPostHandler);
 
 module.exports = router;
