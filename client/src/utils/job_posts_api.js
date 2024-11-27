217d3987ec59d107ba324e44a5765d3c857aec0b
William Nhut Lam <williamntlam@gmail.com>
2024-11-26 22:26:16 -0500

    chore: fix merge conflicts

--- client/src/utils/job_posts_api.js
+++ client/src/utils/job_posts_api.js
@@ -19,4 +19,32 @@ const createJobPost = async (jobPostData) => {
   return result;
 };
 
-module.exports = { createJobPost };
+async function deleteJobPosting(jobPostingId) {
+  try {
+    const response = await fetch(
+      `${REACT_APP_API_BASE_URL}/job_postings/${jobPostingId}`,
+      {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json", // Include headers if needed
+        },
+      },
+    );
+
+    if (!response.ok) {
+      // Handle errors
+      const errorData = await response.json();
+      console.error("Error deleting job posting:", errorData);
+      throw new Error(errorData.error || "Failed to delete job posting");
+    }
+
+    // Handle success
+    console.log("Job posting deleted successfully");
+    return await response.json(); // Return response if needed
+  } catch (error) {
+    console.error("Error:", error.message);
+    throw error;
+  }
+}
+
+module.exports = { createJobPost, deleteJobPosting };
--- server/src/routes/job_posts.js
+++ server/src/routes/job_posts.js
@@ -4,6 +4,8 @@ const router = express.Router();
 
 const addJobPostRequestHandler = require("../controllers/job_posts/addJobPost");
 
+const deleteJobPostHandler = require("../controllers/job_posts/deleteJobPost");
+
 const isLoggedIn = require("../middlewares/auth/isLoggedIn");
 
 /**
@@ -84,5 +86,34 @@ const isLoggedIn = require("../middlewares/auth/isLoggedIn");
  * - isLoggedIn: Ensures that the user is authenticated before allowing the creation of job postings.
  */
 router.post("/", isLoggedIn, addJobPostRequestHandler);
+
+/**
+ * Delete a Job Post and its Associated Job Applications
+ *
+ * Deletes a job post identified by its `job_posting_id` along with all the associated job applications.
+ *
+ * @type DELETE
+ *
+ * @param {string} job_posting_id - The ID of the job posting to delete. This should be passed as a route parameter.
+ *
+ * Example Request:
+ * DELETE /:job_posting_id
+ *
+ * Example Successful Response:
+ * {
+ *   "status": success,
+ *   "message": "Job Posts and all associated Job Applications have been successfully deleted.",
+ *   "data": null
+ * }
+ *
+ * Example Error Response:
+ * {
+ *   "error": error.message
+ * }
+ *
+ * Middleware:
+ * - isLoggedIn: Ensures that the user is authenticated before allowing the deletion operation.
+ */
+router.delete("/:job_posting_id", isLoggedIn, deleteJobPostHandler);
 
 module.exports = router;
