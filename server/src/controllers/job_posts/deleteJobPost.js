72288f53319bb24bcfe24637ed94ef8430909efe
William Nhut Lam <163342709+williamntlam@users.noreply.github.com>
2024-11-26 22:22:02 -0500

    [TCES-47][READY-FOR-REVIEW]: Implement DELETE request for Job Postings (#126)
    * chore: added skeleton structure for DELETE HTTP requests for Job Posts
    
    * feat: implemented logic for deleting a Job Post
    
    * feat: implemented routing for DELETE HTTP request for Job Posts
    
    * feat: configured routing in index.js file
    
    * fix: changed URL from 'job_posts' to 'job_postings' to fit table name
    
    * feat: implemented API call from the frontend for DELETE HTTP request for Job Posts
    
    * feat: resumes are deleted along with Job Applications of related Job Post
    
    * refactor: changed job_posts.js in routes folder to match employer_timeline_entries.js format
    
    * fix: added await for async objects to resolve promise objects and added transaction commits and rollbacks for atomicity
    
    * fix: resolved promise object for deleting S3 files when using array.map
    
    * refactor: removed sequelize import
    
    * feat: deleted fully works now
    
    * feat: delete works now for Job Posts

---
+++ server/src/controllers/job_posts/deleteJobPost.js
@@ -0,0 +1,62 @@
+const JobApplication = require("../../models/job_applications.model");
+const JobPosting = require("../../models/job_posts.model");
+const { deleteFileFromS3 } = require("../../utils/s3");
+
+const deleteJobPostHandler = async (req, res) => {
+  // ! Delete a Job Post and all of its information regardless of close date.
+  // ! Delete any Job Applications associated with this Job Post (i.e job_post_id === given ID in path parameters).
+
+  const { job_posting_id: jobPostId } = req.params;
+
+  // ! If the given job_posting_id is invalid, then return an error.
+
+  if (!jobPostId || Number.isNaN(parseInt(jobPostId, 10))) {
+    return res.status(400).json({ error: "Invalid or missing job_posting_id" });
+  }
+
+  try {
+    const jobPosting = await JobPosting.findByPk(jobPostId);
+
+    // ! Search to see if the Job Post exists.
+
+    // jobPosting will be null if there is no Job Posting found with the corresponding jobPosting Id.
+    if (!jobPosting) {
+      return res.status(404).json({ error: "Job Posting could not be found." });
+    }
+
+    // ! Find all associated Job Applications.
+
+    const jobApplications = await JobApplication.findAll({
+      where: { job_posting_id: jobPostId },
+      attributes: ["resume"],
+    });
+
+    // ! Delete all associated resumes with deleted Job Applications
+
+    await Promise.all(
+      jobApplications.map((app) => deleteFileFromS3(app.resume)),
+    );
+
+    // ! Delete all associated Job Applications with Job Post
+
+    await JobApplication.destroy({
+      where: { job_posting_id: jobPostId },
+    });
+
+    // ! Delete Job Post itself.
+
+    await JobPosting.destroy({
+      where: { id: jobPostId }, // Assuming `id` is the primary key for JobPosting
+    });
+
+    return res.status(200).json({
+      status: "success",
+      message: `Job Posts and all associated Job Applications have been successfully deleted`,
+      data: null,
+    });
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
+};
+
+module.exports = deleteJobPostHandler;
