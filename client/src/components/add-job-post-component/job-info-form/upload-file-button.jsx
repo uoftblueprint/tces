e845b767631d7ed4eec21fcfb75e1a1d53175ac0
William Nhut Lam <williamntlam@gmail.com>
2025-01-06 20:54:38 -0500

    chore: added comments to detail implementation of file upload button

--- client/src/components/add-job-post-component/job-info-form/upload-file-button.jsx
+++ client/src/components/add-job-post-component/job-info-form/upload-file-button.jsx
@@ -1,3 +1,9 @@
 export default function FileUploadButton() {
+  // ! Use the DropzoneArea from 'material-ui-dropzone'
+  // ! Reference the CodeSandbox project.
+
+  // ! Use the S3 bucket upload function to upload the resume file.
+  // ! Need to correctly set the name of the file.
+  // ! The file name should be: `${jobApplication.id}_${name}_${associatedJobPost.title}`,
   return <div>Placeholder</div>;
 }
