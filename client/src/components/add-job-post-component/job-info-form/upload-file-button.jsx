d873a40b6588facd9e2f394e507d0ce8e0a31987
William Nhut Lam <williamntlam@gmail.com>
2025-01-08 11:03:40 -0500

    feat: use prop drilling to make the file upload button functional

--- client/src/components/add-job-post-component/job-info-form/upload-file-button.jsx
+++ client/src/components/add-job-post-component/job-info-form/upload-file-button.jsx
@@ -1,9 +1,19 @@
-export default function FileUploadButton() {
+import PropTypes from "prop-types";
+
+export default function FileUploadButton({ onFileSelect }) {
   // ! Use the DropzoneArea from 'material-ui-dropzone'
   // ! Reference the CodeSandbox project.
+
+  // ! Use prop drilling => Assume that the useState variable referencing the uploaded file is provided
+  // ! as a prop
+  console.log(onFileSelect);
 
   // ! Use the S3 bucket upload function to upload the resume file.
   // ! Need to correctly set the name of the file.
   // ! The file name should be: `${jobApplication.id}_${name}_${associatedJobPost.title}`,
   return <div>Placeholder</div>;
 }
+
+FileUploadButton.propTypes = {
+  onFileSelect: PropTypes.func.isRequired, // Validate the prop
+};
