36424adbfd6ed42f4f3c2b3f17c3ca1d0ae8d03f
Alex Lewis <150391000+alexlewis9@users.noreply.github.com>
2025-01-15 07:32:49 -0500

    [TCES-59 Create a component with the Job Type, Location and Sort buttons (#140)
    * job type, location, status, and sort buttons that open dropdown of radio buttons with different options
    
    * pass the locations as a prop

---
+++ client/src/components/shared/job-posts-sort-menu-component/index.jsx
@@ -0,0 +1,75 @@
+import { useState } from "react";
+import { Button, Menu, MenuItem, Radio, ListItemIcon, ListItemText } from "@mui/material";
+
+function SortMenu() {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [selectedValue, setSelectedValue] = useState("ascending");
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleSelect = (event) => {
+    setSelectedValue(event.target.value);
+  };
+  const handleApply = () => {
+    setAnchorEl(null);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <Button
+        id="sort-button"
+        aria-controls={open ? "sort-menu" : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? "true" : undefined}
+        onClick={handleClick}
+        variant="outlined"
+        startIcon={<img src="/img/sortIcon.svg" alt="sort icon" style={{ width: 20, height: 20 }} />}
+        endIcon={
+          <span style={{ borderLeft: "1px solid #90bce4", height: "100%", marginLeft: "6px", paddingLeft: "14px", paddingRight: "6px", marginTop: "-6px", marginBottom: "-6px" }}>&#9662;</span>
+        }
+      >
+        Sort
+      </Button>
+      <Menu
+        id="sort-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          "aria-labelledby": "sort-button",
+        }}
+      >
+        <MenuItem onClick={(event) => handleSelect(event, "ascending")}>
+          <ListItemIcon>
+            <Radio
+              checked={selectedValue === "ascending"}
+              value="ascending"
+            />
+          </ListItemIcon>
+          <ListItemText primary="Close date: Ascending" />
+        </MenuItem>
+        <MenuItem onClick={(event) => handleSelect(event, "descending")}>
+          <ListItemIcon>
+            <Radio
+              checked={selectedValue === "descending"}
+              value="descending"
+            />
+          </ListItemIcon>
+          <ListItemText primary="Close date: Descending" />
+        </MenuItem>
+        <MenuItem style={{ justifyContent: "right"}}>
+          <Button onClick={handleApply} variant="contained" color="primary" style={{ width: "auto", borderRadius: "12px"}}>
+            Apply
+          </Button>
+        </MenuItem>
+      </Menu>
+    </div>
+  );
+}
+
+export default SortMenu;
---
+++ client/src/components/shared/job-posts-status-menu-component/index.jsx
@@ -0,0 +1,83 @@
+import { useState } from "react";
+import { Button, Menu, MenuItem, Radio, ListItemIcon, ListItemText } from "@mui/material";
+
+function StatusMenu() {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [selectedValue, setSelectedValue] = useState("ascending");
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleSelect = (event) => {
+    setSelectedValue(event.target.value);
+  };
+  const handleApply = () => {
+    setAnchorEl(null);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <Button
+        id="sort-button"
+        aria-controls={open ? "sort-menu" : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? "true" : undefined}
+        onClick={handleClick}
+        variant="outlined"
+        endIcon={
+          <span style={{ borderLeft: "1px solid #90bce4", height: "100%", marginLeft: "6px", paddingLeft: "14px", paddingRight: "6px", marginTop: "-6px", marginBottom: "-6px" }}>&#9662;</span>
+        }
+      >
+        Status
+      </Button>
+      <Menu
+        id="sort-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          "aria-labelledby": "sort-button",
+        }}
+      >
+        <MenuItem onClick={(event) => handleSelect(event, "inactive")}>
+          <ListItemIcon>
+            <Radio
+              checked={selectedValue === "inactive"}
+              value="inactive"
+            />
+          </ListItemIcon>
+          <ListItemText primary="Inactive" />
+        </MenuItem>
+        <MenuItem onClick={(event) => handleSelect(event, "draft")}>
+          <ListItemIcon>
+            <Radio
+              checked={selectedValue === "draft"}
+              value="draft"
+            />
+          </ListItemIcon>
+          <ListItemText primary="Draft" />
+        </MenuItem>
+        <MenuItem onClick={(event) => handleSelect(event, "active")}>
+          <ListItemIcon>
+            <Radio
+              checked={selectedValue === "active"}
+              value="active"
+            />
+          </ListItemIcon>
+          <ListItemText primary="Active" />
+        </MenuItem>
+        <MenuItem style={{ justifyContent: "right"}}>
+          <Button onClick={handleApply} variant="contained" color="primary" style={{ width: "auto", borderRadius: "12px"}}>
+            Apply
+          </Button>
+        </MenuItem>
+      </Menu>
+    </div>
+  );
+}
+
+export default StatusMenu;
