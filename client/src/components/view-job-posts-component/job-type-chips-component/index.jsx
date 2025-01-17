8a372ebdcda8818e8af426b30518e3fcf0fdba12
William Nhut Lam <williamntlam@gmail.com>
2025-01-16 23:37:58 -0500

    chore: run formatter

--- client/src/components/view-job-posts-component/job-type-chips-component/index.jsx
+++ client/src/components/view-job-posts-component/job-type-chips-component/index.jsx
@@ -12,9 +12,7 @@ const chipColors = {
   Internship: { border: "#3F51B5", bg: "#E8EAF6", text: "#000" }, // Indigo
   Inactive: { border: "#424242", bg: "#E0E0E0", text: "#000" }, // Grey
   Draft: { border: "#0057B2", bg: "#E3F2FD", text: "#000" }, // Blue
-  Active: { border: "#1B5E20", bg: "#E8F5E9", text: "#000" } // Green
-
-
+  Active: { border: "#1B5E20", bg: "#E8F5E9", text: "#000" }, // Green
 };
 
 function JobTypeChipsComponent({ jobTypes }) {
