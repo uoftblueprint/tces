dcc23bd349e0882bd1ea2d0a10b1d5aff0c88c8a
William Nhut Lam <williamntlam@gmail.com>
2025-01-23 15:54:51 -0500

    refactor: changed routing to show main page again

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-// import App from "./App";
-import ViewJobPost from "./pages/view-job-post/index";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ViewJobPost />
+    <App />
   </React.StrictMode>,
 );
