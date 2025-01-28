16788b284c5d7b65304f3ba05e631064f16404bf
John Doe <johndoe@email.com>
2025-01-28 18:25:57 -0500

    clear error logs

--- client/src/components/job-postings-dashboard-component/job-postings-dashboard-table/index.jsx
+++ client/src/components/job-postings-dashboard-component/job-postings-dashboard-table/index.jsx
@@ -52,8 +52,6 @@ export default function JobPostingsDashboardTableComponent() {
         });
         const response = await getAllJobPosts(`?${queryParams.toString()}`); 
         const data = await response.json();
-        console.log(data.allJobPosts.data)
-        console.log("query params", queryParams.toString())
         const formattedData = data.allJobPosts.data.map((jobPost) => ({
           id: jobPost.id,
           jobTitle: jobPost.title,
