487495e91ea0052e160ea1749490ee616b177709
lana <lanawehbeh12@gmail.com>
2025-01-28 00:46:27 -0500

    Updated navbar component name

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -31,7 +31,7 @@ import AuthGuard from "./components/wrappers/auth-guard-component";
 
 // data loading wrappers
 import Navbar from "./components/shared/navbar-component/Navbar";
-import PublicNavBar from "./components/shared/navbar-component/PublicNavbar";
+import PublicNavbar from "./components/shared/navbar-component/PublicNavbar";
 import JobLeadDashboard from "./pages/job-lead-dashboard";
 import JobPostingsDashboard from "./pages/job-postings-dashboard";
 import AddJobLeadPage from "./pages/add-job-lead";
@@ -127,7 +127,7 @@ function App() {
           }
         >
           <Route path="/" element={<Navigate to="/job-postings" />} />
-          <Route element={<PublicNavBar />}>
+          <Route element={<PublicNavbar />}>
 
           <Route
             path="/job-postings"
