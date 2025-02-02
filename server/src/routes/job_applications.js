d974a76685f50f1e6f3b45cdc5b8f7a7ac6aec5f
penguinc00kies <jamiesumin@gmail.com>
2025-02-01 22:05:05 -0500

    Added isLoggedIn middleware

--- server/src/routes/job_applications.js
+++ server/src/routes/job_applications.js
@@ -13,28 +13,28 @@ const upload = multer({ dest: path.join(__dirname, "..", "uploads") });
 const router = express.Router();
 
 // Route to get all job applications
-router.get("/", async (req, res) => {
+router.get("/", isLoggedIn, async (req, res) => {
   return getAllJobApplicationsRequestHandler(req, res);
 });
 
 // Route to get job application by id (not job_posting_id since some job_posting_ids are duplicated)
-router.get("/:job_posting_id", async (req, res) => {
+router.get("/:job_posting_id", isLoggedIn, async (req, res) => {
   const jobPostingId = req.params.job_posting_id;
   return getAllJobApplicationsRequestHandler(req, res, jobPostingId);
 });
 
 // Route to get job applications by name
 // NOT CURRENTLY USED
-router.get("/:name", async (req, res) => {
+router.get("/:name", isLoggedIn, async (req, res) => {
   return getOneJobApplicationRequestHandler(req, res);
 });
 
-router.post("/", upload.single("resume"), async (req, res) => {
+router.post("/", isLoggedIn, upload.single("resume"), async (req, res) => {
   return addJobApplicationRequestHandler(req, res);
 });
 
 // Route to get the presigned resume URL for a job application
-router.get("resume/:job_application_id", async (req, res) => {
+router.get("resume/:job_application_id", isLoggedIn, async (req, res) => {
   return getJobApplicationResumeRequestHandler(req, res);
 });
 
@@ -46,9 +46,7 @@ router.get("resume/:job_application_id", async (req, res) => {
  * }
  */
 router.put(
-  "/:job_application_id",
-  isLoggedIn,
-  updateJobApplicationStatusRequestHandler,
+  "/:job_application_id", isLoggedIn, updateJobApplicationStatusRequestHandler,
 );
 
 module.exports = router;
