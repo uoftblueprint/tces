3f1ed4455b81c8f80f28f9f89340e1784e1570d0
Dhairya Thakkar <dhairyathakkar25@gmail.com>
2025-02-08 15:59:43 -0500

    finish the export

--- server/src/controllers/job_posts/getFilterOptions.js
+++ server/src/controllers/job_posts/getFilterOptions.js
@@ -11,47 +11,68 @@ const getFilterOptionsRequestHandler = async (req, res) => {
       .json({ message: "Method not allowed, only GET methods allowed." });
   }
 
-  try { 
-    const { 
-        job_posting_id,
-        name, 
-        email, 
-        jobTitle 
+  try {
+    const { job_posting_id, name, email, jobTitle } = req.query;
 
-    } = req.query; 
+    const baseQuery = {
+      where: {}, // build query attributes
+      include: [
+        {
+          model: JobPosting,
+          attributes: ["id", "title"],
+          where: {
+            state: "Active",
+          },
+        },
+      ],
+      attributes: ["name", "email", "hob_posting_id"],
+    };
 
-    const baseQuery = { 
-        where: {}, 
-        include: [
-            { 
-                model: JobPosting, 
-                attributes: ['id', 'title'], 
-                where: { 
-                    state: 'Active'
-                }
-            }
-        ], 
-        attributes: ['name', 'email', 'hob_posting_id']
-    }; 
-
-    if (job_posting_id) { 
-        baseQuery.where.job_posting_id = job_posting_id;
+    if (job_posting_id) {
+      baseQuery.where.job_posting_id = job_posting_id;
     }
 
-    if (name) { 
-        baseQuery.where.name = {[Op.iLike]: `%${name}}`};  // assuming case insensitive 
+    if (name) {
+      baseQuery.where.name = { [Op.iLike]: `%${name}}` }; // assuming case insensitive
     }
 
-    if (email) { 
-        baseQuery.where.email = {[Op.iLike]: `%${email}`};  // assuming case insensitive 
+    if (email) {
+      baseQuery.where.email = { [Op.iLike]: `%${email}` }; // assuming case insensitive
     }
 
-    if (jobTitle) { 
-        baseQuery[0].include.where = { 
-            ...baseQuery.include[0].where, 
-            title: {[Op.iLike]: `%${jobTitle}`}
-        };
+    if (jobTitle) {
+      baseQuery[0].include.where = {
+        ...baseQuery.include[0].where,
+        title: { [Op.iLike]: `%${jobTitle}` },
+      };
     }
-    const JobApplications = await JobApplication.findAll(baseQuery)
+    const applications = await JobApplication.findAll(baseQuery); // make databse query
+
+    const names = [...new Set(applications.map((app) => app.name))].sort();
+    const emails = [...new Set(applications.map((app) => app.email))].sort();
+    const jobTitles = [
+      ...new Set(applications.map((app) => app.job_posting.title)),
+    ].sort();
+    const jobPostingIds = [
+      ...new Set(applications.map((app) => app.job_posting_id)),
+    ];
+
+    return res.status(200).json({
+      status: "success",
+      data: {
+        names,
+        emails,
+        jobTitles,
+        jobPostingIds,
+      },
+    });
+  } catch (err) {
+    logger.error(`Error in getFilterDropdownOptions: ${err}`);
+    return res.status(500).json({
+      status: "error",
+      message: "An unexpected error occurred",
+    });
   }
+};
 
+module.exports = getFilterDropdownOptionsHandler;
