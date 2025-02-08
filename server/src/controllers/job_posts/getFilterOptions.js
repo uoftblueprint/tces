67fe4b1e170c2a01118240cc6cadbb079aa3e01e
Dhairya Thakkar <dhairyathakkar25@gmail.com>
2025-02-08 15:46:06 -0500

    make base query with params

--- server/src/controllers/job_posts/getFilterOptions.js
+++ server/src/controllers/job_posts/getFilterOptions.js
@@ -11,3 +11,47 @@ const getFilterOptionsRequestHandler = async (req, res) => {
       .json({ message: "Method not allowed, only GET methods allowed." });
   }
 
+  try { 
+    const { 
+        job_posting_id,
+        name, 
+        email, 
+        jobTitle 
+
+    } = req.query; 
+
+    const baseQuery = { 
+        where: {}, 
+        include: [
+            { 
+                model: JobPosting, 
+                attributes: ['id', 'title'], 
+                where: { 
+                    state: 'Active'
+                }
+            }
+        ], 
+        attributes: ['name', 'email', 'hob_posting_id']
+    }; 
+
+    if (job_posting_id) { 
+        baseQuery.where.job_posting_id = job_posting_id;
+    }
+
+    if (name) { 
+        baseQuery.where.name = {[Op.iLike]: `%${name}}`};  // assuming case insensitive 
+    }
+
+    if (email) { 
+        baseQuery.where.email = {[Op.iLike]: `%${email}`};  // assuming case insensitive 
+    }
+
+    if (jobTitle) { 
+        baseQuery[0].include.where = { 
+            ...baseQuery.include[0].where, 
+            title: {[Op.iLike]: `%${jobTitle}`}
+        };
+    }
+    const JobApplications = await JobApplication.findAll(baseQuery)
+  }
+
